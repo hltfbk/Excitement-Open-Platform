4f8cebab57226d82f61e7a0eec3117f3c62d174e
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2012-12-03 13:29:14 +0100

    TreeTagger based LAP added for EN, DE and IT (that adds lemma, token, POS). - eop.lap.dkpro.TreeTaggerEN, TreeTaggerDE, TreeTaggerIT. - Small, minimal test codes also added. - refactored openNLP-DKPro based LAP classes into new package name. (eop.lap.dkpro.*)

---
+++ lap/src/main/java/eu/excitementproject/eop/lap/dkpro/TreeTaggerEN.java
@@ -0,0 +1,70 @@
+package eu.excitementproject.eop.lap.dkpro;
+
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uimafit.factory.AggregateBuilder;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosLemmaTT4J;
+
+import eu.excitementproject.eop.lap.LAPAccess;
+import eu.excitementproject.eop.lap.LAPException;
+import eu.excitementproject.eop.lap.lappoc.LAP_ImplBase;
+
+
+/**
+ * 
+ * English tokenizer + tagger + lemmatizer, that relies on TreeTagger 
+ * (wrapped in DKPro component)  
+ * This class is provides all LAPAccess methods simply by overriding addAnnotationTo() of LAP_ImplBase
+ * 
+ * @author Gil 
+ *
+ */
+
+public class TreeTaggerEN extends LAP_ImplBase implements LAPAccess {
+
+	public TreeTaggerEN() throws LAPException {
+		super(); 		
+		languageIdentifier = "EN"; // set languageIdentifer 
+	}
+
+	@Override
+	public void addAnnotationOn(JCas aJCas, String viewName)
+			throws LAPException {
+		// prepare DKPro components 
+		AnalysisEngineDescription seg = null; 
+		AnalysisEngineDescription tagger = null; 
+		try {
+			seg = createPrimitiveDescription(BreakIteratorSegmenter.class);
+			tagger = createPrimitiveDescription(TreeTaggerPosLemmaTT4J.class);
+		}
+		catch (ResourceInitializationException re)
+		{
+			throw new LAPException("Failed to initilize DKPro UIMA component" ,re ); 
+		}	
+		// Using AggregateBuilder to assign views 
+		AggregateBuilder builder = new AggregateBuilder();
+		builder.add(seg, "_InitialView", viewName);
+		builder.add(tagger, "_InitialView", viewName); 
+		
+		try {
+			AnalysisEngine ae = builder.createAggregate(); 
+			ae.process(aJCas); 
+		}
+		catch (ResourceInitializationException re)
+		{
+			throw new LAPException("Failed to initilize aggregate AE component" ,re ); 
+		} 
+		catch (AnalysisEngineProcessException e) 
+		{
+			throw new LAPException("An exception while running the aggregate AE", e); 
+		}		
+
+	}
+
+}
---
+++ lap/src/main/java/eu/excitementproject/eop/lap/dkpro/TreeTaggerIT.java
@@ -0,0 +1,24 @@
+package eu.excitementproject.eop.lap.dkpro;
+
+import eu.excitementproject.eop.lap.LAPException;
+
+/**
+ * 
+ * Italian tokenizer + tagger + lemmatizer, that relies on TreeTagger 
+ * (wrapped in DKPro component)  
+ * 
+ * TreeTagger DKPro component also supports Italian, with Italian model. 
+ * The class reuses EN code, but just set the langID as IT. 
+ *  
+ * @author Gil 
+ *
+ */
+
+public class TreeTaggerIT extends TreeTaggerEN {
+
+	public TreeTaggerIT() throws LAPException {
+		super(); 
+		languageIdentifier = "IT"; // setting language identifier 
+	}
+
+}
