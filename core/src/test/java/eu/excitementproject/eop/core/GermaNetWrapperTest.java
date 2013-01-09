95370794989a3695b8a2c389f78bb6e3b5128f17
brittazeller <zeller@cl.uni-heidelberg.de>
2013-01-09 17:20:41 +0100

    complete and correct version of GermaNetWrapperTest

--- core/src/test/java/eu/excitementproject/eop/core/GermaNetWrapperTest.java
+++ core/src/test/java/eu/excitementproject/eop/core/GermaNetWrapperTest.java
@@ -3,15 +3,8 @@ package eu.excitementproject.eop.core;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.text.AnnotationIndex;
-import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
 import org.junit.Assume;
 import org.junit.Test;
 
@@ -22,12 +15,9 @@ import eu.excitementproject.eop.core.component.lexicalknowledge.germanet.GermaNe
 import eu.excitementproject.eop.core.component.lexicalknowledge.germanet.GermaNetNotInstalledException;
 import eu.excitementproject.eop.core.component.lexicalknowledge.germanet.GermaNetRelation;
 import eu.excitementproject.eop.core.component.lexicalknowledge.germanet.GermaNetWrapper;
-import eu.excitementproject.eop.core.representation.parsetree.GermanPartOfSpeech;
-import eu.excitementproject.eop.core.representation.parsetree.UnsupportedPosTagStringException;
-import eu.excitementproject.eop.lap.LAPAccess;
-import eu.excitementproject.eop.lap.LAPException;
-import eu.excitementproject.eop.lap.dkpro.TreeTaggerDE;
-import eu.excitementproject.eop.lap.lappoc.ExampleLAP;
+import eu.excitementproject.eop.common.representation.parsetree.GermanPartOfSpeech;
+import eu.excitementproject.eop.common.representation.parsetree.UnsupportedPosTagStringException;
+
 
 
 /**
@@ -56,45 +46,6 @@ public class GermaNetWrapperTest {
 		}
 		Assume.assumeNotNull(gnw); // if gnw is null, the following tests will not be run. 
 
-		//TODO: introduce UIMA structure, check for POS tag, call GN resource for all
-		// ADJ, N*, V*
-		
-		// test UIMA-structured input		
-		LAPAccess lap = null;
-		JCas jcas = null;  
-        try {
-        	lap = new TreeTaggerDE();  //TODO: is "TreeTaggerDE" correct?
-        	jcas = lap.generateSingleTHPairCAS("Die Obstindustrie hat Einbrüche erlitten.", "Es werden kaum mehr Äpfel gekauft.");
-        } catch (LAPException e) {
-        	System.err.println(e.getMessage()); 
-        }
-        
-		try {
-			AnnotationIndex<Annotation> annoIdx = 
-					jcas.getView("TextView").getAnnotationIndex(); // getSofa().getCAS().getAnnotationIndex();
-			FSIterator<Annotation> annoIter = annoIdx.iterator();
-			
-			while (annoIter.hasNext()) {
-				// TODO: how can I decide which type of annotation I want (token, lemma...)?
-				Annotation currentAnno = annoIter.next();
-				
-				System.out.println(currentAnno.getType().getName());
-				System.out.println(currentAnno.getCoveredText());
-			}
-			
-			jcas.getView("HypothesisView");
-			
-			//TODO: as soon as I achieve the singular annotation values, check for each 
-			// annotation of a Token if the POS is ADJ, N* or V*
-			// if yes: call gnw.
-			
-			
-		} catch (CASException e) {
-			System.err.println(e.getMessage());
-		}
-		
-		
-		
 		
 		// Test for verbs
 		try{
@@ -111,6 +62,7 @@ public class GermaNetWrapperTest {
 		{
 			e.printStackTrace(); 
 		}		
+		
 		
 		// Negative test for verbs to show the given POS is used
 		try{
