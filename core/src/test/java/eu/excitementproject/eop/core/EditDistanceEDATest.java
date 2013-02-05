907292d7b90a7cb16df7f6f2b209f58bd5c99223
Roberto Zanoli <zanoli@norris.fbk.eu>
2013-02-05 13:35:20 +0100

    Add Configurations and models for Italian. Minor changes to EdiDistance algortihm and component.

--- core/src/test/java/eu/excitementproject/eop/core/EditDistanceEDATest.java
+++ core/src/test/java/eu/excitementproject/eop/core/EditDistanceEDATest.java
@@ -1,13 +1,15 @@
 package eu.excitementproject.eop.core;
 
 import org.apache.uima.jcas.JCas;
-//import org.junit.Test;
+import org.junit.Test;
+import org.junit.Ignore;
 //import static org.junit.Assert.*;
 //import eu.excitementproject.eop.core.component.distance.CasCreation;
 //import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
-import java.io.File;
+import java.util.List;
+import java.io.*;
 
 
 import org.apache.uima.cas.FSIterator;
@@ -24,7 +26,8 @@ import eu.excitementproject.eop.lap.PlatformCASProber;
 
 public class EditDistanceEDATest {
 
-	//@Test
+	@Ignore
+	@Test
 	public void test() {
     	
 		/*
@@ -58,12 +61,21 @@ public class EditDistanceEDATest {
 		
 		try {
 		
-			File configFile = new File("./src/main/resources/ITA_EditDistance_TokenEditDistance_configuration_file.xml");
+			//No lexical resources
+			File configFile = new File("./src/main/resources/configuration-file/EditDistanceEDA_NonLexRes_IT.xml");
+			File annotatedFileName = new File("./src/main/resources/results/EditDistanceEDA_NonLexRes_IT.xml_Result.txt");
+			String evaluationFileName = "./src/main/resources/results/EditDistanceEDA_NonLexRes_IT.xml_Result.txt_Eval.xml";
+			
+			//Wordnet
+			//File configFile = new File("./src/main/resources/configuration-file/EditDistanceEDA_Wordnet_IT.xml");
+			//File annotatedFileName = new File("./src/main/resources/results/EditDistanceEDA_Wordnet_IT.xml_Result.txt");
+			//String evaluationFileName = "./src/main/resources/results/EditDistanceEDA_Wordnet_IT.xml_Result.txt_Eval.xml";
+			
 			CommonConfig config = new ImplCommonConfig(configFile);
 		
-			editDistanceEDA.setTrain(true);
+			//editDistanceEDA.setTrain(true);
 			editDistanceEDA.initialize(config);
-			editDistanceEDA.startTraining(config);
+			//editDistanceEDA.startTraining(config);
 			
 			File testDir = null;
 			testDir = new File("./target/IT/test/");
@@ -71,17 +83,21 @@ public class EditDistanceEDATest {
 				if (!xmi.getName().endsWith(".xmi")) {
 					continue;
 				}
-				JCas cas = PlatformCASProber.probeXmi(xmi, System.out);
-				//JCas cas = PlatformCASProber.probeXmi(xmi, null);
+				//JCas cas = PlatformCASProber.probeXmi(xmi, System.out);
+				JCas cas = PlatformCASProber.probeXmi(xmi, null);
 				IEditDistanceTEDecision teDecision1 = editDistanceEDA.process(cas);
 				// System.err.println(teDecision1.getDecision().toString()) ;
 				//System.err.println(teDecision1.getDecision().toString());
-				System.err.println(getPairID(cas) + "\t" + getGoldLabel(cas) + "\t"  + teDecision1.getDecision().toString() + "\t" + teDecision1.getConfidence());
+				//System.err.println(getPairID(cas) + "\t" + getGoldLabel(cas) + "\t"  + teDecision1.getDecision().toString() + "\t" + teDecision1.getConfidence());
+				
 				list.add(getPairID(cas) + "\t" + getGoldLabel(cas) + "\t"  + teDecision1.getDecision().toString() + "\t" + teDecision1.getConfidence());
 			    
 			}
 			
-			scorer(list);
+			save(annotatedFileName, list, false);
+			EDAScorer.score(annotatedFileName, evaluationFileName);
+			
+			//scorer(list);
 			/*
 			IEditDistanceTEDecision teDecision1 = editDistanceEDA.process(casList.get(0));
 			// System.err.println(teDecision1.getDecision().toString()) ;
@@ -98,6 +114,8 @@ public class EditDistanceEDATest {
 		}
 		
     }
+	
+	
 	
 	/**
 	 * @param aCas
@@ -128,6 +146,64 @@ public class EditDistanceEDATest {
 			return p.getGoldAnswer();
 		}
 	}
+	
+	
+	/** Save text in the specified text file
+     * @param fileName name of the file
+     * @param text Text to be saved
+     * @exception IOException
+     */
+    public void save(String fileName, String text, boolean append) throws Exception {
+    	
+    	BufferedWriter writer = null;
+    	
+    	try {
+    		
+    		//creo un oggetto FileWriter...
+	    	// ... che incapsulo in un BufferedWriter...
+	    	writer = new BufferedWriter(new FileWriter(fileName, append));
+	    	// ... che incapsulo in un PrintWriter
+	    	PrintWriter printout = new PrintWriter(writer);
+	    	printout.print(text);
+	    	printout.close();
+	    	
+    	} catch (Exception e) {
+    		//System.err.println(e.getMessage());
+    		throw new Exception(e.getMessage());
+    	} finally {
+    		if (writer != null)
+    			writer.close();
+    	}
+
+    }
+    
+    
+    public void save(File file, List<String> list, boolean append) throws Exception {
+    	
+    	BufferedWriter writer = null;
+    	
+    	try {
+    		
+	    	writer = new BufferedWriter(new FileWriter(file));
+	    	// ... che incapsulo in un PrintWriter
+	    	PrintWriter printout = new PrintWriter(writer, append);
+    	
+	    	Iterator<String> iterator = list.iterator();
+	    	while(iterator.hasNext()) {
+	    		printout.println(iterator.next());
+	    	}
+	    	printout.close();
+	    	
+    	} catch (Exception e) {
+    		System.err.println(e.getMessage());
+    		throw new Exception(e.getMessage());
+    	} finally {
+    		if (writer != null)
+    			writer.close();
+    	}
+
+    }
+	
 	
 	//taken for the code provided by RUI
 	public void scorer(ArrayList<String> list) {
