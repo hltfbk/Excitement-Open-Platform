1523c5ea936783ad729a99742269aee971f18a76
Julia Kreutzer <julia@Kunigunde.(none)>
2013-03-21 18:49:01 +0100

    extended GermanDistTest for 10k distributional data

--- core/src/test/java/eu/excitementproject/eop/core/GermanDistSimTest.java
+++ core/src/test/java/eu/excitementproject/eop/core/GermanDistSimTest.java
@@ -26,9 +26,10 @@ public class GermanDistSimTest {
 	@Test 
 	public void test() /* throws java.lang.Exception */ {
 		
-		GermanDistSim gds = null;
+		//for 1k dewak-distributional data
+		GermanDistSim gds1 = null;
 		try {
-			gds = new GermanDistSim("src/main/resources/dewakdistributional-data");
+			gds1 = new GermanDistSim("src/main/resources/dewakdistributional-data");
 		}
 		catch (GermanDistSimNotInstalledException e) {
 			System.out.println("WARNING: GermanDistSim files are not found. Please install them properly, and pass its location correctly to the component.");
@@ -39,10 +40,10 @@ public class GermanDistSimTest {
 			e.printStackTrace(); 
 		}
 
-		Assume.assumeNotNull(gds); 
+		Assume.assumeNotNull(gds1); 
 		
 		try {
-			for (LexicalRule<? extends GermanDistSimInfo> rule : gds.getRulesForLeft("sie", null)) {
+			for (LexicalRule<? extends GermanDistSimInfo> rule : gds1.getRulesForLeft("sie", null)) {
 				assertTrue(rule.getLLemma().equals("sie"));
 				assertFalse(rule.getRLemma().equals(""));
 				assertFalse(rule.getRelation().equals(""));
@@ -55,12 +56,12 @@ public class GermanDistSimTest {
 		}
 
 		/* Testing init via CommonConfig */ 
-		gds=null; 
+		gds1=null; 
 		try {
 			// An example configuration file that only holds two German lexical component
 			// configuration. 
 			File f = new File("./src/test/resources/german_resource_test_configuration.xml");
-			gds = new GermanDistSim(new ImplCommonConfig(f)); 
+			gds1 = new GermanDistSim(new ImplCommonConfig(f)); 
 		}
 		catch (GermanDistSimNotInstalledException e) {
 			System.out.println("WARNING: GermanDistSim files are not found. Please install them properly, and pass its location correctly in the CommonConfig, that is passed to the component.");
@@ -70,11 +71,73 @@ public class GermanDistSimTest {
 		{
 			e.printStackTrace(); 
 		}
-		assertNotNull(gds); 
+		assertNotNull(gds1); 
 		//Assume.assumeNotNull(gds); // Let's assume the file is correct. no need. 
 		
 		try {
-			for (LexicalRule<? extends GermanDistSimInfo> rule : gds.getRulesForLeft("sie", null)) {
+			for (LexicalRule<? extends GermanDistSimInfo> rule : gds1.getRulesForLeft("sie", null)) {
+				assertTrue(rule.getLLemma().equals("sie"));
+				assertFalse(rule.getRLemma().equals(""));
+				assertFalse(rule.getRelation().equals(""));
+				assertTrue(rule.getConfidence() > 0);
+			}
+		}
+		catch (LexicalResourceException e)
+		{
+			e.printStackTrace(); 
+		}
+		
+		
+		//for 10k dewak-distributional data
+		GermanDistSim gds10 = null;
+		try {
+			gds10 = new GermanDistSim("src/main/resources/dewakdistributional-data-10k");
+		}
+		catch (GermanDistSimNotInstalledException e) {
+			System.out.println("WARNING: GermanDistSim files are not found. Please install them properly, and pass its location correctly to the component.");
+			//throw e;
+		}
+		catch (BaseException e)
+		{
+			e.printStackTrace(); 
+		}
+
+		Assume.assumeNotNull(gds10); 
+		
+		try {
+			for (LexicalRule<? extends GermanDistSimInfo> rule : gds10.getRulesForLeft("sie", null)) {
+				assertTrue(rule.getLLemma().equals("sie"));
+				assertFalse(rule.getRLemma().equals(""));
+				assertFalse(rule.getRelation().equals(""));
+				assertTrue(rule.getConfidence() > 0);
+			}
+		}
+		catch (LexicalResourceException e)
+		{
+			e.printStackTrace(); 
+		}
+
+		/* Testing init via CommonConfig */ 
+		gds10=null; 
+		try {
+			// An example configuration file that only holds two German lexical component
+			// configuration. 
+			File f = new File("./src/test/resources/german_resource_test_configuration.xml");
+			gds10 = new GermanDistSim(new ImplCommonConfig(f)); 
+		}
+		catch (GermanDistSimNotInstalledException e) {
+			System.out.println("WARNING: GermanDistSim files are not found. Please install them properly, and pass its location correctly in the CommonConfig, that is passed to the component.");
+			//throw e;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(); 
+		}
+		assertNotNull(gds10); 
+		//Assume.assumeNotNull(gds10); // Let's assume the file is correct. no need. 
+		
+		try {
+			for (LexicalRule<? extends GermanDistSimInfo> rule : gds10.getRulesForLeft("sie", null)) {
 				assertTrue(rule.getLLemma().equals("sie"));
 				assertFalse(rule.getRLemma().equals(""));
 				assertFalse(rule.getRelation().equals(""));
