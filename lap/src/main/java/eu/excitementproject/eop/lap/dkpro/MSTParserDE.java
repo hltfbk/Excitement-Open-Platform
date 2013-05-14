810587a79579d2eba6834f2d269b626783b90be1
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-05-14 20:03:58 +0200

    - LAP_ImplBaseAE: Improved how parameters are passed to underlying AEs. (Now you can really select models by passing arguments in IMplBaseAE constructor.) - Improved MaltParser LAP and MSTParser LAP according to this IMPLBASEAE improvements.

--- lap/src/main/java/eu/excitementproject/eop/lap/dkpro/MSTParserDE.java
+++ lap/src/main/java/eu/excitementproject/eop/lap/dkpro/MSTParserDE.java
@@ -1,5 +1,7 @@
 package eu.excitementproject.eop.lap.dkpro;
 
+import java.util.Map;
+
 import eu.excitementproject.eop.lap.LAPException;
 
 /**
@@ -11,8 +13,6 @@ import eu.excitementproject.eop.lap.LAPException;
  * It will use default German Tagger model, but you can select Parser model by the "variant" string in the constructor. 
  * (variant, is a model ID, that is used by DKPro parser AE) 
  * 
- * // TODO: "Long" model, yet to be provided. (for the moment, only "default" variant is in the repository) 
- * // TODO: test "Variant" actually works for long, or any other models. 
  * @author Gil 
  *
  */
@@ -20,7 +20,6 @@ import eu.excitementproject.eop.lap.LAPException;
 
 public class MSTParserDE extends MSTParserEN {
 
-	
 	/**
 	 * Without any argument, this will initiate the instance with German default 
 	 * model for MST Parser, with the capability of annotating the "three views" (Default, TextView & HypothesisView). 
@@ -37,7 +36,7 @@ public class MSTParserDE extends MSTParserEN {
 	
 	/**
 	 * <P> 
-	 * This constructor will initiate this MSTParser based LAP instance with the given modelVarient. 
+	 * This constructor will initiate this MSTParser based LAP instance with the given PARSER_MODEL_VARIANT. 
 	 * (Thus: de.tudarmstadt.ukp.dkpro.core.mstparser-model-parser-[LANGID]-[modelVarient] ) 
 	 * 
 	 * <P> 
@@ -45,14 +44,13 @@ public class MSTParserDE extends MSTParserEN {
 	 * If you need only need to annotate a specific view (e.g. Default only, not for T & H View, etc) 
 	 * call the full constructor with two arguments. (e.g. if you give one view only, its initialization is faster).
 	 * 
-	 * @param modelVarient String of the model varient. If the given varient is not accessible in the class path, the underlying AE will try to use default. If default isn't there, it will raise an exception.
+	 * @param listAEDescriptorsArgs (For German, "long" and "default" model variants are included in the first release.) This argument holds the arguments that will be passed directly to the listAEDescriptorsArgs() which will generate AE descriptors. The arguments are depending on the implementation of listAEDescriptors(). Only one argument is know to this pipeline. PARSER_MODEL_VARIANT: which will load corresponding model variant of MST parser. The value can be null, for default arguments. 
 	 * @throws LAPException
 	 */
-	public MSTParserDE(String modelVariant) throws LAPException 
+	public MSTParserDE(Map<String,String> listAEDescriptorsArgs) throws LAPException 
 	{
-		super(modelVariant);
+		super(listAEDescriptorsArgs);
 		this.languageIdentifier="DE"; // setting LangID. Other things will be handled accordingly in base class
-		
 	}
 	
 	/**
@@ -60,13 +58,13 @@ public class MSTParserDE extends MSTParserEN {
 	 * Full constructor, that accepts model Varient and view names. If you use this LAP for EOP EDAs (e.g. where you need TextView & Hypothesis View), you can simply call the constructor without view names (since their default is Default, TView and HView). 
 	 * ModelVarient will load the desinated model-varient. 	(Thus: de.tudarmstadt.ukp.dkpro.core.mstparser-model-parser-[LANGID]-[modelVarient] ) 
 	 * 
-	 * @param modelVarient String of the model varient. If the given varient is not accessible in the class path, the underlying AE will try to use default. If default isn't there, it will raise an exception.
 	 * @param views String array of "view names". Once initialized, the instance can only annotate this "previously given" Views only. (See LAP_ImplBaseAE for explanation). 
+	 * @param listAEDescriptorsArgs (For German, "long" and "default" model variants are included in the first release.) This argument holds the arguments that will be passed directly to the listAEDescriptorsArgs() which will generate AE descriptors. The arguments are depending on the implementation of listAEDescriptors(). Only one argument is know to this pipeline. PARSER_MODEL_VARIANT: which will load corresponding model variant of MST parser. 
 	 * @throws LAPException
 	 */
-	public MSTParserDE(String modelVariant, String[] views) throws LAPException
+	public MSTParserDE(String[] views, Map<String,String> listAEDescriptorsArgs ) throws LAPException
 	{
-		super(modelVariant, views); 
+		super(views, listAEDescriptorsArgs); 
 		this.languageIdentifier="DE"; // setting LangID. Other things will be handled accordingly in base class
 	}
 	
