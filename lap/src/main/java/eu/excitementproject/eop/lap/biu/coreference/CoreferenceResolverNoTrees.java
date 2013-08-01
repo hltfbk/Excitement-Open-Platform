f3eac263be9eab6ae972326845010b86d97f2382
oferbr <oferbr@gmail.com>
2013-08-01 20:10:36 +0300

    create the "no trees" version of coref resolver (and arkref specifically)

--- lap/src/main/java/eu/excitementproject/eop/lap/biu/coreference/CoreferenceResolverNoTrees.java
+++ lap/src/main/java/eu/excitementproject/eop/lap/biu/coreference/CoreferenceResolverNoTrees.java
@@ -2,81 +2,74 @@ package eu.excitementproject.eop.lap.biu.coreference;
 
 import java.util.List;
 
-import eu.excitementproject.eop.common.representation.coreference.TreeCoreferenceInformation;
-import eu.excitementproject.eop.common.representation.parse.tree.dependency.basic.BasicNode;
+import eu.excitementproject.eop.common.representation.coreference.DockedMention;
 
 
 /**
- * Resolves co-reference relations, and creates a ??TODO??? that
- * represents those co-reference relations.
+ * Resolves co-reference relations, and creates a list of {@link DockedMention} lists
+ * that represents those co-reference relations. Each list represents one coreference group,
+ * all docked mentions in the list are part of the same coreference group.
  * 
  * <P>
- * Usage: TODO
+ * Usage:
  * <OL>
  * <LI>Call {@link #init()}</LI>
- * <LI>Call {@link #SetInput(List, String)}</LI>
+ * <LI>Call {@link #SetInput(String)}</LI>
  * <LI>Call {@link #resolve()}</LI>
  * <LI>Get the co-reference information by calling the method {@link #getCoreferenceInformation()}</LI>
  * <LI>Repeat the setInput, resolve and getCoreferenceInformation as many times as required.
  * When done, and coreference resolver is not to be used any more, call {@link #cleanUp()}</LI>
  * </OL>
  * 
- * Based on {@link CoreferenceResolver}.
+ * <B>NOTE</B> This is based on {@link CoreferenceResolver}. The main difference is that
+ * this class does not connect the coreference information to trees, but rather to offsets
+ * on the input text. The main motivation here is UIMA.
  * 
  * 
  * @author Ofer Bronstein
- *
- * @param <S> the type of the tree's nodes. For example: {@link BasicNode}.
+ * @since August 2013
  */
-public abstract class CoreferenceResolverNoTrees<S>
+public abstract class CoreferenceResolverNoTrees
 {
 	public abstract void init() throws CoreferenceResolutionException;
 	public abstract void cleanUp();
 	
 	/**
-	 * 
-	 * @param trees a list of parse trees of the text. The list must be ordered by the sentences' order,
-	 * i.e. the first item in the list should be the parse tree of the first sentence in the text, the
-	 * second the second, etc.
-	 * @param originalText The original text.
+	 * @param originalText The original text, e.g. a full document.
 	 * @throws CoreferenceResolutionException
 	 */
-	public void setInput(List<S> trees, String originalText) throws CoreferenceResolutionException
+	public void setInput(String originalText) throws CoreferenceResolutionException
 	{
-		if (null==trees) throw new CoreferenceResolutionException("null==trees");
 		if (null==originalText) throw new CoreferenceResolutionException("null==originalText");
-		this.trees = trees;
 		this.originalText = originalText;
 		resolved = false;
 	}
 	
 	/**
 	 * Calls a coreference resolution system that resolves the coreference relations,
-	 * and creates {@link TreeCoreferenceInformation} that represents the coreference system's output.
+	 * and creates a list of {@link DockedMention} lists that represents the coreference system's output.
 	 * That information can be retrieved later by calling {@link #getCoreferenceInformation()}.
 	 * @throws CoreferenceResolutionException
 	 */
 	public void resolve() throws CoreferenceResolutionException
 	{
 		if (resolved) throw new CoreferenceResolutionException("Resolution already done.");
-		if (null==trees) throw new CoreferenceResolutionException("You did not call setInput()");
 		if (null==originalText) throw new CoreferenceResolutionException("You did not call setInput()");
 		
 		implementResolve();
 		resolved = true;
 	}
 	
-	public TreeCoreferenceInformation<S> getCoreferenceInformation() throws CoreferenceResolutionException
+	public List<List<DockedMention>> getCoreferenceInformation() throws CoreferenceResolutionException
 	{
 		if (!resolved) throw new CoreferenceResolutionException("Not yet resolved.");
-		return this.coreferenceInformation;
+		return this.dockedMentions;
 	}
 	
 	protected abstract void implementResolve() throws CoreferenceResolutionException;
 	
 	
-	protected List<S> trees = null;
 	protected String originalText = null;
-	protected TreeCoreferenceInformation<S> coreferenceInformation = null;
+	protected List<List<DockedMention>> dockedMentions = null;
 	protected boolean resolved = false;
 }
