57e2ad5b334da2c11239d8d8c5b8fb8115b0cc07
oferbr <oferbr@gmail.com>
2013-08-04 16:52:19 +0300

    improve tests

--- lap/src/test/java/eu/excitementproject/eop/lap/biu/en/coreference/arkref/ArkrefCoreferenceResolversTests.java
+++ lap/src/test/java/eu/excitementproject/eop/lap/biu/en/coreference/arkref/ArkrefCoreferenceResolversTests.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 
 import eu.excitementproject.eop.common.representation.coreference.DockedMention;
 import eu.excitementproject.eop.common.representation.coreference.TreeCoreferenceInformation;
+import eu.excitementproject.eop.common.representation.parse.tree.AbstractNodeUtils;
 import eu.excitementproject.eop.common.representation.parse.tree.dependency.basic.BasicNode;
-import eu.excitementproject.eop.lap.biu.ae.BiuTreeBuilder;
 import eu.excitementproject.eop.lap.biu.coreference.CoreferenceResolutionException;
 import eu.excitementproject.eop.lap.biu.coreference.CoreferenceResolver;
 import eu.excitementproject.eop.lap.biu.coreference.CoreferenceResolverNoTrees;
@@ -19,6 +19,8 @@ import eu.excitementproject.eop.lap.biu.en.tokenizer.TokenizerException;
 import eu.excitementproject.eop.lap.biu.ner.NamedEntityRecognizerException;
 import eu.excitementproject.eop.lap.biu.postagger.PosTaggerException;
 import eu.excitementproject.eop.lap.biu.sentencesplit.SentenceSplitterException;
+import eu.excitementproject.eop.lap.biu.test.BiuTestUtils;
+import eu.excitementproject.eop.lap.biu.test.BiuTreeBuilder;
 
 /**
  * Test both arkref coreference resolvers: {@link ArkrefCoreferenceResolver}
@@ -29,14 +31,15 @@ import eu.excitementproject.eop.lap.biu.sentencesplit.SentenceSplitterException;
  */
 public class ArkrefCoreferenceResolversTests {
 	@BeforeClass
-	public static void beforeClass() {
-		int x = 8; // Warning to remember to use the "assume" thing
+	public static void beforeClass() throws IOException {
+		// Run tests only under BIU environment
+		BiuTestUtils.assumeBiuEnvironment();
 	}
 	
 	private void assertBothCorefsAgree(String text) throws ArkrefClientException, IOException, SentenceSplitterException, TokenizerException, PosTaggerException, NamedEntityRecognizerException, ParserRunException, CoreferenceResolutionException{
+
 		BiuTreeBuilder builder = new BiuTreeBuilder();
 		List<BasicNode> referenceTrees = builder.buildTrees(text);
-		//System.out.printf("TEXT:\n%s\n\n", ...);
 
 		CoreferenceResolver<BasicNode> coref1 = new ArkrefCoreferenceResolver();
 		coref1.init();
@@ -51,6 +54,28 @@ public class ArkrefCoreferenceResolversTests {
 		coref2.resolve();
 		List<List<DockedMention>> dockedCorefInfo = coref2.getCoreferenceInformation();
 		coref2.cleanUp();
+		
+		
+		// Prints
+		System.out.printf("TEXT:\n%s\n\n", text);
+
+		System.out.printf("TEXT TREES:\n");
+		for (BasicNode tree : referenceTrees) {
+			System.out.printf("%s\n", AbstractNodeUtils.getIndentedString(tree));
+		}
+		System.out.printf("\n\n");
+
+		System.out.printf("TREE COREFERENCE INFORMATION:\n%s\n\n", treeCorefInfo.toStringWriteSubtrees());
+
+		System.out.printf("DOCKED COREFERENCE INFORMATION:\n");
+		for (List<DockedMention> dockedMentions: dockedCorefInfo) {
+			System.out.printf("%s\n", dockedMentions);
+		}
+		
+		System.out.printf("\n==================================\n");
+
+		int x=8;
+		throw new CoreferenceResolutionException("Test no complete yet");
 	}
 	
 	@Test
