25ebd69a5631f6bebb1527f1c3e27f5ad7d2f44d
Asher Stern <astern7@gmail.com>
2013-08-08 17:03:43 +0300

    Add code for PASTA based gap mode

--- biutee/src/main/java/eu/excitementproject/eop/biutee/rteflow/macro/gap/GapToolBoxFactory.java
+++ biutee/src/main/java/eu/excitementproject/eop/biutee/rteflow/macro/gap/GapToolBoxFactory.java
@@ -1,22 +1,19 @@
 package eu.excitementproject.eop.biutee.rteflow.macro.gap;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import static eu.excitementproject.eop.biutee.utilities.ConfigurationParametersNames.RTE_ENGINE_GAP_HYBRID_MODE;
 
 import org.apache.log4j.Logger;
 
-import eu.excitementproject.eop.biutee.classifiers.ClassifierException;
-import eu.excitementproject.eop.biutee.classifiers.LinearClassifier;
-import eu.excitementproject.eop.common.representation.parse.tree.TreeAndParentMap;
+import eu.excitementproject.eop.biutee.rteflow.macro.gap.pastabased.PastaBasedGapToolsFactory;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationException;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationFile;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationParams;
+import eu.excitementproject.eop.lap.biu.en.pasta.PredicateArgumentStructureBuilderFactory;
+import eu.excitementproject.eop.lap.biu.en.pasta.PredicateArgumentStructureBuilderFactoryFactory;
+import eu.excitementproject.eop.lap.biu.pasta.identification.PredicateArgumentIdentificationException;
 import eu.excitementproject.eop.transformations.alignment.AlignmentCriteria;
 import eu.excitementproject.eop.transformations.representation.ExtendedInfo;
 import eu.excitementproject.eop.transformations.representation.ExtendedNode;
-import static eu.excitementproject.eop.biutee.utilities.ConfigurationParametersNames.RTE_ENGINE_GAP_HYBRID_MODE;
 /**
  * A factory of {@link GapToolBox}.
  * 
@@ -26,6 +23,9 @@ import static eu.excitementproject.eop.biutee.utilities.ConfigurationParametersN
  */
 public class GapToolBoxFactory
 {
+	/////////////// PUBLIC ///////////////
+
+	
 	public GapToolBoxFactory(ConfigurationFile configurationFile,
 			ConfigurationParams configurationParams,
 			AlignmentCriteria<ExtendedInfo, ExtendedNode> alignmentCriteria)
@@ -40,13 +40,7 @@ public class GapToolBoxFactory
 	{
 		try
 		{
-			boolean hybridMode = false;
-			if (configurationParams.containsKey(RTE_ENGINE_GAP_HYBRID_MODE))
-			{
-				hybridMode = configurationParams.getBoolean(RTE_ENGINE_GAP_HYBRID_MODE);
-			}
-			
-			if (hybridMode)
+			if (isHybridMode())
 			{
 				logger.info("In hybrid gap mode.");
 				return createGapToolBoxForHybridMode();
@@ -54,20 +48,7 @@ public class GapToolBoxFactory
 			else
 			{
 				logger.info("In pure transformation mode.");
-				return new GapToolBox<ExtendedInfo, ExtendedNode>()
-				{
-					@Override
-					public boolean isHybridMode() throws GapException
-					{
-						return false;
-					}
-					
-					@Override
-					public GapToolsFactory<ExtendedInfo, ExtendedNode> getGapToolsFactory() throws GapException
-					{
-						return null;
-					}
-				};
+				return createGapToolBoxForPureTransformationMode();
 			}
 		}
 		catch(ConfigurationException e)
@@ -78,10 +59,41 @@ public class GapToolBoxFactory
 	}
 	
 	
+	/////////////// PRIVATE ///////////////
+	
+	private boolean isHybridMode() throws ConfigurationException
+	{
+		boolean hybridMode = false;
+		if (configurationParams.containsKey(RTE_ENGINE_GAP_HYBRID_MODE))
+		{
+			hybridMode = configurationParams.getBoolean(RTE_ENGINE_GAP_HYBRID_MODE);
+		}
+		return hybridMode;
+	}
+	
+	private GapToolBox<ExtendedInfo, ExtendedNode> createGapToolBoxForPureTransformationMode() throws GapException
+	{
+		return new GapToolBox<ExtendedInfo, ExtendedNode>()
+		{
+			@Override
+			public boolean isHybridMode() throws GapException
+			{
+				return false;
+			}
+			
+			@Override
+			public GapToolsFactory<ExtendedInfo, ExtendedNode> getGapToolsFactory() throws GapException
+			{
+				return null;
+			}
+		};
+	}
+	
 	private GapToolBox<ExtendedInfo, ExtendedNode> createGapToolBoxForHybridMode() throws GapException
 	{
-		// create a GapToolBox. This tool box is global to the system, and is able to
-		// construct tools for specific hypotheses.
+		final PredicateArgumentStructureBuilderFactory<ExtendedInfo, ExtendedNode> builderFactory = createPredArgsFactory();
+		final GapToolsFactory<ExtendedInfo, ExtendedNode> gapToolsFactory = new PastaBasedGapToolsFactory<ExtendedInfo, ExtendedNode>(builderFactory);
+		
 		return new GapToolBox<ExtendedInfo, ExtendedNode>()
 		{
 			@Override
@@ -93,80 +105,109 @@ public class GapToolBoxFactory
 			@Override
 			public GapToolsFactory<ExtendedInfo, ExtendedNode> getGapToolsFactory() throws GapException
 			{
-				// The GapToolsFactory constructs tools for specific hypotheses.
-				return new GapToolsFactory<ExtendedInfo, ExtendedNode>()
-				{
-					@Override
-					public GapToolInstances<ExtendedInfo, ExtendedNode> createInstances(
-							final TreeAndParentMap<ExtendedInfo, ExtendedNode> hypothesis,
-							final LinearClassifier classifierForSearch) throws GapException
-					{
-						// Create a GapFeatureVectorGenerator - the one that really does
-						// the job. It returns the feature vector which resembels the
-						// gap between the parse trees.
-						final GapFeatureVectorGenerator gapFeatureVectorGenerator = new GapFeatureVectorGenerator(alignmentCriteria);
-						
-						final GapFeaturesUpdate<ExtendedInfo, ExtendedNode> gapFeaturesUpdate = new GapFeaturesUpdate<ExtendedInfo, ExtendedNode>()
-						{
-							@Override
-							public Map<Integer, Double> updateForGap(
-									TreeAndParentMap<ExtendedInfo, ExtendedNode> tree,
-									Map<Integer, Double> featureVector) throws GapException
-							{
-								Map<Integer, Double> ret = new LinkedHashMap<>();
-								Map<Integer, Double> gapVector = gapFeatureVectorGenerator.createFeatureVector(tree, hypothesis);
-								Set<Integer> features = new LinkedHashSet<>();
-								features.addAll(featureVector.keySet());
-								features.addAll(gapVector.keySet());
-								for (Integer feature : features)
-								{
-									double value = 0.0;
-									if (featureVector.get(feature)!=null) value+=featureVector.get(feature);
-									if (gapVector.get(feature)!=null) value+=gapVector.get(feature);
-									ret.put(feature,value);
-								}
-								return ret;
-							}
-						};
-						
-						GapHeuristicMeasure<ExtendedInfo, ExtendedNode> gapHeuristicMeasure = new GapHeuristicMeasure<ExtendedInfo, ExtendedNode>()
-						{
-							@Override
-							public double measure(TreeAndParentMap<ExtendedInfo, ExtendedNode> tree, Map<Integer, Double> featureVector) throws GapException
-							{
-								Map<Integer, Double> featureVectorWithGap = gapFeaturesUpdate.updateForGap(tree,featureVector);
-								try{return (-classifierForSearch.getProduct(featureVectorWithGap));}
-								catch(ClassifierException e){throw new GapException("Failed to calculate gap measure, due to a problem in the classifier.",e);}
-							}
-						};
-						
-						return new GapToolInstances<>(gapFeaturesUpdate, gapHeuristicMeasure);
-						
-					}
-				};
+				return gapToolsFactory;
 			}
 		};
 	}
+
 	
-//	private PredicateArgumentStructureBuilderFactory<ExtendedInfo, ExtendedNode> createPredArgsFactory() throws GapException
+	private PredicateArgumentStructureBuilderFactory<ExtendedInfo, ExtendedNode> createPredArgsFactory() throws GapException
+	{
+		try
+		{
+			PredicateArgumentStructureBuilderFactoryFactory<ExtendedInfo, ExtendedNode> factoryFactory =
+					new PredicateArgumentStructureBuilderFactoryFactory<ExtendedInfo, ExtendedNode>(configurationFile);
+			return factoryFactory.createBuilderFactory();
+		}
+		catch (ConfigurationException | PredicateArgumentIdentificationException e)
+		{
+			throw new GapException("Failed to build predicate argument structure builder factory.");
+		}
+	}
+
+	
+	
+	
+	
+	
+//	private GapToolBox<ExtendedInfo, ExtendedNode> createGapToolBoxForHybridModeOld() throws GapException
 //	{
-//		try
+//		// create a GapToolBox. This tool box is global to the system, and is able to
+//		// construct tools for specific hypotheses.
+//		return new GapToolBox<ExtendedInfo, ExtendedNode>()
 //		{
-//			PredicateArgumentStructureBuilderFactoryFactory<ExtendedInfo, ExtendedNode> factoryFactory =
-//					new PredicateArgumentStructureBuilderFactoryFactory<ExtendedInfo, ExtendedNode>(configurationFile);
-//			return factoryFactory.createBuilderFactory();
-//		}
-//		catch (ConfigurationException | PredicateArgumentIdentificationException e)
-//		{
-//			throw new GapException("Failed to build predicate argument structure builder factory.");
-//		}
+//			@Override
+//			public boolean isHybridMode() throws GapException
+//			{
+//				return true;
+//			}
+//			
+//			@Override
+//			public GapToolsFactory<ExtendedInfo, ExtendedNode> getGapToolsFactory() throws GapException
+//			{
+//				// The GapToolsFactory constructs tools for specific hypotheses.
+//				return new GapToolsFactory<ExtendedInfo, ExtendedNode>()
+//				{
+//					@Override
+//					public GapToolInstances<ExtendedInfo, ExtendedNode> createInstances(
+//							final TreeAndParentMap<ExtendedInfo, ExtendedNode> hypothesis,
+//							final LinearClassifier classifierForSearch) throws GapException
+//					{
+//						// Create a GapFeatureVectorGenerator - the one that really does
+//						// the job. It returns the feature vector which resembels the
+//						// gap between the parse trees.
+//						final GapFeatureVectorGenerator gapFeatureVectorGenerator = new GapFeatureVectorGenerator(alignmentCriteria);
+//						
+//						final GapFeaturesUpdate<ExtendedInfo, ExtendedNode> gapFeaturesUpdate = new GapFeaturesUpdate<ExtendedInfo, ExtendedNode>()
+//						{
+//							@Override
+//							public Map<Integer, Double> updateForGap(
+//									TreeAndParentMap<ExtendedInfo, ExtendedNode> tree,
+//									Map<Integer, Double> featureVector) throws GapException
+//							{
+//								Map<Integer, Double> ret = new LinkedHashMap<>();
+//								Map<Integer, Double> gapVector = gapFeatureVectorGenerator.createFeatureVector(tree, hypothesis);
+//								Set<Integer> features = new LinkedHashSet<>();
+//								features.addAll(featureVector.keySet());
+//								features.addAll(gapVector.keySet());
+//								for (Integer feature : features)
+//								{
+//									double value = 0.0;
+//									if (featureVector.get(feature)!=null) value+=featureVector.get(feature);
+//									if (gapVector.get(feature)!=null) value+=gapVector.get(feature);
+//									ret.put(feature,value);
+//								}
+//								return ret;
+//							}
+//						};
+//						
+//						GapHeuristicMeasure<ExtendedInfo, ExtendedNode> gapHeuristicMeasure = new GapHeuristicMeasure<ExtendedInfo, ExtendedNode>()
+//						{
+//							@Override
+//							public double measure(TreeAndParentMap<ExtendedInfo, ExtendedNode> tree, Map<Integer, Double> featureVector) throws GapException
+//							{
+//								Map<Integer, Double> featureVectorWithGap = gapFeaturesUpdate.updateForGap(tree,featureVector);
+//								try{return (-classifierForSearch.getProduct(featureVectorWithGap));}
+//								catch(ClassifierException e){throw new GapException("Failed to calculate gap measure, due to a problem in the classifier.",e);}
+//							}
+//						};
+//						
+//						return new GapToolInstances<>(gapFeaturesUpdate, gapHeuristicMeasure);
+//						
+//					}
+//				};
+//			}
+//		};
 //	}
+
 	
 	
 	
-	@SuppressWarnings("unused")
+	
+	
 	private final ConfigurationFile configurationFile;
 	private final ConfigurationParams configurationParams;
+	@SuppressWarnings("unused")
 	private final AlignmentCriteria<ExtendedInfo, ExtendedNode> alignmentCriteria;
 	
 	
--- biutee/src/main/java/eu/excitementproject/eop/biutee/rteflow/macro/gap/pastabased/PastaBasedGapToolsFactory.java
+++ biutee/src/main/java/eu/excitementproject/eop/biutee/rteflow/macro/gap/pastabased/PastaBasedGapToolsFactory.java
@@ -1,5 +1,7 @@
 package eu.excitementproject.eop.biutee.rteflow.macro.gap.pastabased;
 
+import java.util.Set;
+
 import eu.excitementproject.eop.biutee.classifiers.LinearClassifier;
 import eu.excitementproject.eop.biutee.rteflow.macro.gap.GapException;
 import eu.excitementproject.eop.biutee.rteflow.macro.gap.GapToolInstances;
@@ -7,6 +9,10 @@ import eu.excitementproject.eop.biutee.rteflow.macro.gap.GapToolsFactory;
 import eu.excitementproject.eop.common.representation.parse.representation.basic.Info;
 import eu.excitementproject.eop.common.representation.parse.tree.AbstractNode;
 import eu.excitementproject.eop.common.representation.parse.tree.TreeAndParentMap;
+import eu.excitementproject.eop.common.representation.pasta.PredicateArgumentStructure;
+import eu.excitementproject.eop.lap.biu.en.pasta.PredicateArgumentStructureBuilderFactory;
+import eu.excitementproject.eop.lap.biu.pasta.identification.PredicateArgumentIdentificationException;
+import eu.excitementproject.eop.lap.biu.pasta.identification.PredicateArgumentStructureBuilder;
 
 /**
  * 
@@ -18,11 +24,33 @@ import eu.excitementproject.eop.common.representation.parse.tree.TreeAndParentMa
  */
 public class PastaBasedGapToolsFactory<I extends Info, S extends AbstractNode<I, S>> implements GapToolsFactory<I, S> 
 {
+	public PastaBasedGapToolsFactory(
+			PredicateArgumentStructureBuilderFactory<I, S> builderFactory)
+	{
+		super();
+		this.builderFactory = builderFactory;
+	}
+
 	@Override
 	public GapToolInstances<I, S> createInstances(TreeAndParentMap<I, S> hypothesis, LinearClassifier classifierForSearch) throws GapException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			PredicateArgumentStructureBuilder<I, S> builder = builderFactory.createBuilder(hypothesis);
+			builder.build();
+			Set<PredicateArgumentStructure<I, S>> hypothesisStructures = builder.getPredicateArgumentStructures();
+
+			PastaBasedGapFeaturesUpdate<I,S> pastaBasedUpdate = new PastaBasedGapFeaturesUpdate<I,S>(
+					builderFactory,hypothesisStructures,hypothesis,classifierForSearch);
+
+			return new GapToolInstances<>(pastaBasedUpdate, pastaBasedUpdate);
+		}
+		catch (PredicateArgumentIdentificationException e)
+		{
+			throw new GapException("Failed to build predicate argument structures for the hypothesis.",e);
+		}
+
 	}
+	
+	private final PredicateArgumentStructureBuilderFactory<I, S> builderFactory;
 
 }
