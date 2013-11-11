d13357ee6a6cc476fbf6460cfb56dc0b493449d2
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2013-11-11 11:33:21 +0100

    added TestLemmaPosSimilarityGerman class: for German lexical resource test, etc.

--- distsim/src/main/java/eu/excitementproject/eop/distsim/resource/TestLemmaPosSimilarity.java
+++ distsim/src/main/java/eu/excitementproject/eop/distsim/resource/TestLemmaPosSimilarity.java
@@ -9,17 +9,13 @@ import eu.excitementproject.eop.common.component.lexicalknowledge.LexicalRule;
 import eu.excitementproject.eop.common.component.lexicalknowledge.RuleInfo;
 import eu.excitementproject.eop.common.representation.partofspeech.ByCanonicalPartOfSpeech;
 import eu.excitementproject.eop.common.representation.partofspeech.CanonicalPosTag;
-import eu.excitementproject.eop.common.representation.partofspeech.GermanPartOfSpeech;
 import eu.excitementproject.eop.common.representation.partofspeech.UnsupportedPosTagStringException;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationException;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationFile;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationFileDuplicateKeyException;
 import eu.excitementproject.eop.common.utilities.configuration.ConfigurationParams;
 import eu.excitementproject.eop.distsim.storage.ElementTypeException;
-import eu.excitementproject.eop.distsim.storage.RedisBasedStringListBasicMap;
 import eu.excitementproject.eop.distsim.storage.SimilarityNotFoundException;
-import eu.excitementproject.eop.distsim.storage.SimilarityStorage;
-import eu.excitementproject.eop.distsim.storage.DefaultSimilarityStorage;
 import eu.excitementproject.eop.distsim.util.Configuration;
 
 /**
@@ -35,48 +31,36 @@ public class TestLemmaPosSimilarity {
 	
 	public static void main(String[] args) throws SimilarityNotFoundException, LexicalResourceException, UnsupportedPosTagStringException, ConfigurationFileDuplicateKeyException, ConfigurationException, ElementTypeException {
 		
-//		if (args.length != 1) {
-//			System.out.println("Usage: eu.excitementproject.eop.distsim.application.TestLemmaPosSimilarity <configuration file>");
-//			System.exit(0);
-//		}
+		if (args.length != 1) {
+			System.out.println("Usage: eu.excitementproject.eop.distsim.application.TestLemmaPosSimilarity <configuration file>");
+			System.exit(0);
+		}
 		
-//		ConfigurationFile confFile = new ConfigurationFile(args[0]);
-//		ConfigurationParams confParams = confFile.getModuleConfiguration(Configuration.KNOWLEDGE_RESOURCE);
+		ConfigurationFile confFile = new ConfigurationFile(args[0]);
+		ConfigurationParams confParams = confFile.getModuleConfiguration(Configuration.KNOWLEDGE_RESOURCE);
 
-//		LexicalResource<? extends RuleInfo> resource = new SimilarityStorageBasedLexicalResource(confParams);
+		LexicalResource<? extends RuleInfo> resource = new SimilarityStorageBasedLexicalResource(confParams);
 		
-		RedisBasedStringListBasicMap l2r = new RedisBasedStringListBasicMap("gillespie.cl.uni-heidelberg.de", 6379);
-		RedisBasedStringListBasicMap r2l = new RedisBasedStringListBasicMap("gillespie.cl.uni-heidelberg.de", 6380); 
+		List<? extends LexicalRule<? extends RuleInfo>> similarities = resource.getRulesForLeft("affect",null);
+		System.out.println("left-2-right rules for affect: ");
+		for (LexicalRule<? extends RuleInfo> similarity : similarities)
+			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
 		
-		SimilarityStorage ss = new DefaultSimilarityStorage(l2r, r2l, "GermanLin", "inst1", "eu.excitementproject.eop.distsim.items.LemmaPosBasedElement"); 
-		LexicalResource<? extends RuleInfo> resource = new SimilarityStorageBasedLexicalResource(ss);
-
-		List<? extends LexicalRule<? extends RuleInfo>> similarities = resource.getRulesForLeft("ort", null); 
-		
-		System.out.println("left-2-right rules: ");
+		similarities = resource.getRulesForRight("affect",null);
+		System.out.println("\nright-2-left rules for affect: ");
 		for (LexicalRule<? extends RuleInfo> similarity : similarities)
 			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
 
-//		List<? extends LexicalRule<? extends RuleInfo>> similarities = resource.getRulesForLeft("affect",null);
-//		System.out.println("left-2-right rules for affect: ");
-//		for (LexicalRule<? extends RuleInfo> similarity : similarities)
-//			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
-//		
-//		similarities = resource.getRulesForRight("affect",null);
-//		System.out.println("\nright-2-left rules for affect: ");
-//		for (LexicalRule<? extends RuleInfo> similarity : similarities)
-//			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
-//
-//	
-//		similarities = resource.getRulesForLeft("affect",new ByCanonicalPartOfSpeech(CanonicalPosTag.V.name()));
-//		System.out.println("\nleft-2-right rules for affect as a verb: ");
-//		for (LexicalRule<? extends RuleInfo> similarity : similarities)
-//			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
-//
-//		similarities = resource.getRulesForRight("affect",new ByCanonicalPartOfSpeech(CanonicalPosTag.V.name()));
-//		System.out.println("\nright-2-left rules for affect as a verb: ");
-//		for (LexicalRule<? extends RuleInfo> similarity : similarities)
-//			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
+	
+		similarities = resource.getRulesForLeft("affect",new ByCanonicalPartOfSpeech(CanonicalPosTag.V.name()));
+		System.out.println("\nleft-2-right rules for affect as a verb: ");
+		for (LexicalRule<? extends RuleInfo> similarity : similarities)
+			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
+
+		similarities = resource.getRulesForRight("affect",new ByCanonicalPartOfSpeech(CanonicalPosTag.V.name()));
+		System.out.println("\nright-2-left rules for affect as a verb: ");
+		for (LexicalRule<? extends RuleInfo> similarity : similarities)
+			System.out.println("<" + similarity.getLLemma() + "," + similarity.getLPos() + ">" + " --> " + "<" + similarity.getRLemma() + "," + similarity.getRPos() + ">" + ": " + similarity.getConfidence());
 
 	}
 }
