67ac5b540c8f908ce33283bae657cba96365e9df
Roberto Zanoli <zanoli@norris.fbk.eu>
2014-01-07 13:14:14 +0100

    Enable test for EditDistanceEDATest, EditDistancePSOEDATest
    Enable test for OpneNLP tagger
    
    Create a small data set (i.e. a portion of the RTE-3 data set) for English, German
    and Italian data set to test Laps and EDAs.

--- core/src/test/java/eu/excitementproject/eop/core/LapTest.java
+++ core/src/test/java/eu/excitementproject/eop/core/LapTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 
 import org.junit.Test;
-import org.junit.Ignore; 
 
 import eu.excitementproject.eop.lap.LAPAccess;
 import eu.excitementproject.eop.lap.LAPException;
@@ -14,12 +13,11 @@ import eu.excitementproject.eop.lap.LAPException;
 import eu.excitementproject.eop.lap.dkpro.*;
 
 /**
-* Tests LAPs
-* It annotates 3 small data sets by using OpenNLP tagger 
+* This class tests OpenNLP tagger for English, German and Italian on a small portion
+* of the RTE-3 data set.
 */
 public class LapTest {
 		
-	    @Ignore
 		@Test
 		public void test() {
 			testLAP_IT();
@@ -34,11 +32,11 @@ public class LapTest {
 			File outputDir = null;
 			
 			// generate XMI files for the training data
-			inputFile = new File("./src/test/resources/data-set/Italian_dev_small.xml");
+			inputFile = new File("./src/test/resources/data-set/ITA/Italian_dev_small.xml");
 			//inputFile = new File("./src/test/resources/data-set/Italian_dev.xml");
 			assertTrue(inputFile.exists());
-			//outputDir = new File("./src/test/resources/data-set/ITA/dev/");
-			outputDir = new File("/tmp/ITA/dev/");
+			outputDir = new File("./src/test/resources/data-set/ITA/dev/");
+			//outputDir = new File("/tmp/ITA/dev/");
 		
 			if (!outputDir.exists()) {
 				outputDir.mkdirs();
@@ -55,11 +53,11 @@ public class LapTest {
 			}
 			
 			// generate XMI files for the testing data
-			inputFile = new File("./src/test/resources/data-set/Italian_test_small.xml");
+			inputFile = new File("./src/test/resources/data-set/ITA/Italian_test_small.xml");
 			//inputFile = new File("./src/test/resources/data-set/Italian_test.xml");
 			assertTrue(inputFile.exists());
-			//outputDir = new File("./src/test/resources/data-set/ITA/test/");
-			outputDir = new File("/tmp/ITA/test/");
+			outputDir = new File("./src/test/resources/data-set/ITA/test/");
+			//outputDir = new File("/tmp/ITA/test/");
 			
 			if (!outputDir.exists()) {
 				outputDir.mkdirs();
@@ -80,11 +78,11 @@ public class LapTest {
 			File outputDir = null;
 			
 			// generate XMI files for the training data
-			inputFile = new File("./src/test/resources/data-set/English_dev_small.xml");
+			inputFile = new File("./src/test/resources/data-set/ENG/English_dev_small.xml");
 			//inputFile = new File("./src/test/resources/data-set/English_dev.xml");
 			assertTrue(inputFile.exists());
-			//outputDir = new File("./src/test/resources/data-set/ENG/dev/");
-			outputDir = new File("/tmp/ENG/dev/");
+			outputDir = new File("./src/test/resources/data-set/ENG/dev/");
+			//outputDir = new File("/tmp/ENG/dev/");
 		
 			if (!outputDir.exists()) {
 				outputDir.mkdirs();
@@ -101,11 +99,11 @@ public class LapTest {
 			}
 			
 			// generate XMI files for the testing data
-			inputFile = new File("./src/test/resources/data-set/English_test_small.xml");
+			inputFile = new File("./src/test/resources/data-set/ENG/English_test_small.xml");
 			//inputFile = new File("./src/test/resources/data-set/English_test.xml");
 			assertTrue(inputFile.exists());
-			//outputDir = new File("./src/test/resources/data-set/ENG/test/");
-			outputDir = new File("/tmp/ENG/test/");
+			outputDir = new File("./src/test/resources/data-set/ENG/test/");
+			//outputDir = new File("/tmp/ENG/test/");
 			
 			if (!outputDir.exists()) {
 				outputDir.mkdirs();
@@ -126,11 +124,11 @@ public class LapTest {
 			File outputDir = null;
 			
 			// generate XMI files for the training data
-			inputFile = new File("./src/test/resources/data-set/German_dev_small.xml");
+			inputFile = new File("./src/test/resources/data-set/GER/German_dev_small.xml");
 			//inputFile = new File("./src/test/resources/data-set/German_dev.xml");
 			assertTrue(inputFile.exists());
-			//outputDir = new File("./src/test/resources/data-set/GER/dev/");
-			outputDir = new File("/tmp/GER/dev/");
+			outputDir = new File("./src/test/resources/data-set/GER/dev/");
+			//outputDir = new File("/tmp/GER/dev/");
 		
 			if (!outputDir.exists()) {
 				outputDir.mkdirs();
@@ -147,11 +145,11 @@ public class LapTest {
 			}
 			
 			// generate XMI files for the testing data
-			inputFile = new File("./src/test/resources/data-set/German_test_small.xml");
+			inputFile = new File("./src/test/resources/data-set/GER/German_test_small.xml");
 			//inputFile = new File("./src/test/resources/data-set/German_test.xml");
 			assertTrue(inputFile.exists());
-			//outputDir = new File("./src/test/resources/data-set/GER/test/");
-			outputDir = new File("/tmp/GER/test/");
+			outputDir = new File("./src/test/resources/data-set/GER/test/");
+			//outputDir = new File("/tmp/GER/test/");
 			
 			if (!outputDir.exists()) {
 				outputDir.mkdirs();
