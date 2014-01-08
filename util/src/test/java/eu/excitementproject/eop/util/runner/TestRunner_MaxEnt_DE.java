6df74bfc26423daa5d78d90b94a363e158b1f42c
Roberto Zanoli <zanoli@norris.fbk.eu>
2014-01-08 10:21:53 +0100

    Enable tests on Edit Distance and TIE EDA
    These tests are conducted by using the Runner class being able to call those EDAs
    so that EDAs can be tested when they are called in the same manner.

--- util/src/test/java/eu/excitementproject/eop/util/runner/TestRunner_MaxEnt_DE.java
+++ util/src/test/java/eu/excitementproject/eop/util/runner/TestRunner_MaxEnt_DE.java
@@ -5,15 +5,22 @@ import org.junit.*;
 // test for German with MaxEnt
 public class TestRunner_MaxEnt_DE {
 
-	@Ignore
 	@Test
 	public void test() {
-		String[] cmd = {"-config", "./src/test/resources/configuration-file/demo_config_test_maxent_DE.xml",
+		String[] cmd = {"-config", "../core/src/test/resources/configuration-file/MaxEntClassificationEDA_Base+OpenNLP_DE.xml",
 				        "-train",
-				        "-trainFile","./src/test/resources/data-set/de_demo_dev.xml",
+				        "-trainFile","../core/src/test/resources/data-set/GER/German_dev_small.xml",
 				        "-test",
-				        "-testFile","./src/test/resources/data-set/de_demo_test.xml",
-				        "-output","./src/test/resources/results/"};
+				        "-testFile","../core/src/test/resources/data-set/GER/German_test_small.xml",
+				        "-output","../core/src/test/resources/results/"};
+		/*
+		String[] cmd = {"-config", "./src/test/resources/configuration-file/demo_config_test_maxent_DE.xml",
+                "-train",
+                "-trainFile","./src/test/resources/data-set/de_demo_dev.xml",
+                "-test",
+                "-testFile","./src/test/resources/data-set/de_demo_test.xml",
+                "-output","./src/test/resources/results/"};
+		*/
 		try {
 			EOPRunner runner = new EOPRunner(cmd);
 			runner.run();
