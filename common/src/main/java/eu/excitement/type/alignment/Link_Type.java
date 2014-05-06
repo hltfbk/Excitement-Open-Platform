017337db12d405a3e0d824bf2d13b1ee32bdc3b5
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-05-06 15:58:09 +0200

    updated alignment.Link types, according to v3 proposal

--- common/src/main/java/eu/excitement/type/alignment/Link_Type.java
+++ common/src/main/java/eu/excitement/type/alignment/Link_Type.java
@@ -15,12 +15,15 @@ import org.apache.uima.jcas.tcas.Annotation_Type;
 
 /** - CAS type that links two Target. 
 - Multi-view type: a Link connects one target in T (TextView), the other target in H (HypothesisView). 
-- Three features: "from" (alignment.Target), "to" (alignment.Target), and "strength" (double). 
 
-The semantic of a "Link" is: The text (or structure) pointed by "from" has a relation of "type" to the text (or structure) pointed in "to". 
+The semantic of a "Link" is: The texts (or structures) pointed by "TSideTarget" and "HSideTarget" have a relation of "type", with the direction of "direction",  on a strength of "strength". 
 
- We make no assumptions regarding what annotations are aligned by Link and Target types. One Target can be linked by an arbitrary number of Link, also a Target can group an arbitrary number of Annotations. Note that uima.tcas.Annotation is the super type of almost all CAS annotation data. Since a Target can group Annotation, it can group any type of annotations in CAS. 
- * Updated by JCasGen Thu Apr 24 15:21:08 CEST 2014
+We make no assumptions regarding what annotations are aligned by Link and Target types. One Target can be linked by an arbitrary number of Link, also a Target can group an arbitrary number of Annotations. Note that uima.tcas.Annotation is the super type of almost all CAS annotation data. Since a Target can group Annotation, it can group any type of annotations in CAS.
+
+Some notes on Link type usage. (Indexing and setting begin - end) 
+- A Link instance should be indexed on the Hypothesis View. So one iteration over the Hypothesis view can get all alignment links.
+- begin and end : both span value should hold the same value to that of HSide Target 
+ * Updated by JCasGen Tue May 06 15:54:34 CEST 2014
  * @generated */
 public class Link_Type extends Annotation_Type {
   /** @generated */
@@ -51,38 +54,38 @@ public class Link_Type extends Annotation_Type {
   public final static boolean featOkTst = JCasRegistry.getFeatOkTst("eu.excitement.type.alignment.Link");
  
   /** @generated */
-  final Feature casFeat_from;
+  final Feature casFeat_TSideTarget;
   /** @generated */
-  final int     casFeatCode_from;
+  final int     casFeatCode_TSideTarget;
   /** @generated */ 
-  public int getFrom(int addr) {
-        if (featOkTst && casFeat_from == null)
-      jcas.throwFeatMissing("from", "eu.excitement.type.alignment.Link");
-    return ll_cas.ll_getRefValue(addr, casFeatCode_from);
+  public int getTSideTarget(int addr) {
+        if (featOkTst && casFeat_TSideTarget == null)
+      jcas.throwFeatMissing("TSideTarget", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getRefValue(addr, casFeatCode_TSideTarget);
   }
   /** @generated */    
-  public void setFrom(int addr, int v) {
-        if (featOkTst && casFeat_from == null)
-      jcas.throwFeatMissing("from", "eu.excitement.type.alignment.Link");
-    ll_cas.ll_setRefValue(addr, casFeatCode_from, v);}
+  public void setTSideTarget(int addr, int v) {
+        if (featOkTst && casFeat_TSideTarget == null)
+      jcas.throwFeatMissing("TSideTarget", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setRefValue(addr, casFeatCode_TSideTarget, v);}
     
   
  
   /** @generated */
-  final Feature casFeat_to;
+  final Feature casFeat_HSideTarget;
   /** @generated */
-  final int     casFeatCode_to;
+  final int     casFeatCode_HSideTarget;
   /** @generated */ 
-  public int getTo(int addr) {
-        if (featOkTst && casFeat_to == null)
-      jcas.throwFeatMissing("to", "eu.excitement.type.alignment.Link");
-    return ll_cas.ll_getRefValue(addr, casFeatCode_to);
+  public int getHSideTarget(int addr) {
+        if (featOkTst && casFeat_HSideTarget == null)
+      jcas.throwFeatMissing("HSideTarget", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getRefValue(addr, casFeatCode_HSideTarget);
   }
   /** @generated */    
-  public void setTo(int addr, int v) {
-        if (featOkTst && casFeat_to == null)
-      jcas.throwFeatMissing("to", "eu.excitement.type.alignment.Link");
-    ll_cas.ll_setRefValue(addr, casFeatCode_to, v);}
+  public void setHSideTarget(int addr, int v) {
+        if (featOkTst && casFeat_HSideTarget == null)
+      jcas.throwFeatMissing("HSideTarget", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setRefValue(addr, casFeatCode_HSideTarget, v);}
     
   
  
@@ -103,6 +106,96 @@ public class Link_Type extends Annotation_Type {
     ll_cas.ll_setDoubleValue(addr, casFeatCode_strength, v);}
     
   
+ 
+  /** @generated */
+  final Feature casFeat_direction;
+  /** @generated */
+  final int     casFeatCode_direction;
+  /** @generated */ 
+  public String getDirection(int addr) {
+        if (featOkTst && casFeat_direction == null)
+      jcas.throwFeatMissing("direction", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_direction);
+  }
+  /** @generated */    
+  public void setDirection(int addr, String v) {
+        if (featOkTst && casFeat_direction == null)
+      jcas.throwFeatMissing("direction", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setStringValue(addr, casFeatCode_direction, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_alignerID;
+  /** @generated */
+  final int     casFeatCode_alignerID;
+  /** @generated */ 
+  public String getAlignerID(int addr) {
+        if (featOkTst && casFeat_alignerID == null)
+      jcas.throwFeatMissing("alignerID", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_alignerID);
+  }
+  /** @generated */    
+  public void setAlignerID(int addr, String v) {
+        if (featOkTst && casFeat_alignerID == null)
+      jcas.throwFeatMissing("alignerID", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setStringValue(addr, casFeatCode_alignerID, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_version;
+  /** @generated */
+  final int     casFeatCode_version;
+  /** @generated */ 
+  public String getVersion(int addr) {
+        if (featOkTst && casFeat_version == null)
+      jcas.throwFeatMissing("version", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_version);
+  }
+  /** @generated */    
+  public void setVersion(int addr, String v) {
+        if (featOkTst && casFeat_version == null)
+      jcas.throwFeatMissing("version", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setStringValue(addr, casFeatCode_version, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_info;
+  /** @generated */
+  final int     casFeatCode_info;
+  /** @generated */ 
+  public String getInfo(int addr) {
+        if (featOkTst && casFeat_info == null)
+      jcas.throwFeatMissing("info", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_info);
+  }
+  /** @generated */    
+  public void setInfo(int addr, String v) {
+        if (featOkTst && casFeat_info == null)
+      jcas.throwFeatMissing("info", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setStringValue(addr, casFeatCode_info, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_groupLabel;
+  /** @generated */
+  final int     casFeatCode_groupLabel;
+  /** @generated */ 
+  public int getGroupLabel(int addr) {
+        if (featOkTst && casFeat_groupLabel == null)
+      jcas.throwFeatMissing("groupLabel", "eu.excitement.type.alignment.Link");
+    return ll_cas.ll_getRefValue(addr, casFeatCode_groupLabel);
+  }
+  /** @generated */    
+  public void setGroupLabel(int addr, int v) {
+        if (featOkTst && casFeat_groupLabel == null)
+      jcas.throwFeatMissing("groupLabel", "eu.excitement.type.alignment.Link");
+    ll_cas.ll_setRefValue(addr, casFeatCode_groupLabel, v);}
+    
+  
 
 
 
@@ -113,16 +206,36 @@ public class Link_Type extends Annotation_Type {
     casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
 
  
-    casFeat_from = jcas.getRequiredFeatureDE(casType, "from", "eu.excitement.type.alignment.Target", featOkTst);
-    casFeatCode_from  = (null == casFeat_from) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_from).getCode();
+    casFeat_TSideTarget = jcas.getRequiredFeatureDE(casType, "TSideTarget", "eu.excitement.type.alignment.Target", featOkTst);
+    casFeatCode_TSideTarget  = (null == casFeat_TSideTarget) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_TSideTarget).getCode();
 
  
-    casFeat_to = jcas.getRequiredFeatureDE(casType, "to", "eu.excitement.type.alignment.Target", featOkTst);
-    casFeatCode_to  = (null == casFeat_to) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_to).getCode();
+    casFeat_HSideTarget = jcas.getRequiredFeatureDE(casType, "HSideTarget", "eu.excitement.type.alignment.Target", featOkTst);
+    casFeatCode_HSideTarget  = (null == casFeat_HSideTarget) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_HSideTarget).getCode();
 
  
     casFeat_strength = jcas.getRequiredFeatureDE(casType, "strength", "uima.cas.Double", featOkTst);
     casFeatCode_strength  = (null == casFeat_strength) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_strength).getCode();
+
+ 
+    casFeat_direction = jcas.getRequiredFeatureDE(casType, "direction", "eu.excitement.type.alignment.Direction", featOkTst);
+    casFeatCode_direction  = (null == casFeat_direction) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_direction).getCode();
+
+ 
+    casFeat_alignerID = jcas.getRequiredFeatureDE(casType, "alignerID", "uima.cas.String", featOkTst);
+    casFeatCode_alignerID  = (null == casFeat_alignerID) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_alignerID).getCode();
+
+ 
+    casFeat_version = jcas.getRequiredFeatureDE(casType, "version", "uima.cas.String", featOkTst);
+    casFeatCode_version  = (null == casFeat_version) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_version).getCode();
+
+ 
+    casFeat_info = jcas.getRequiredFeatureDE(casType, "info", "uima.cas.String", featOkTst);
+    casFeatCode_info  = (null == casFeat_info) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_info).getCode();
+
+ 
+    casFeat_groupLabel = jcas.getRequiredFeatureDE(casType, "groupLabel", "uima.cas.StringList", featOkTst);
+    casFeatCode_groupLabel  = (null == casFeat_groupLabel) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_groupLabel).getCode();
 
   }
 }
