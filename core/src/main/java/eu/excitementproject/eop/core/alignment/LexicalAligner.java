83b89888e65be9941499cb4848794379ee2dd0d6
vered1986 <vered1986@gmail.com>
2014-05-27 17:14:12 +0300

    Implement phase 1 of the LexicalAligner - search the lexical resources for rules t->h of each phrase t in the text and phrase h in the hypothesis.

---
+++ core/src/main/java/eu/excitementproject/eop/core/alignment/LexicalAligner.java
@@ -0,0 +1,411 @@
+package eu.excitementproject.eop.core.alignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.cas.CASException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
+import org.uimafit.util.JCasUtil;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import eu.excitement.type.alignment.Link;
+import eu.excitement.type.alignment.Link.Direction;
+import eu.excitement.type.alignment.Target;
+import eu.excitementproject.eop.common.component.alignment.AlignmentComponent;
+import eu.excitementproject.eop.common.component.lexicalknowledge.LexicalResource;
+import eu.excitementproject.eop.common.component.lexicalknowledge.LexicalResourceCloseException;
+import eu.excitementproject.eop.common.component.lexicalknowledge.LexicalResourceException;
+import eu.excitementproject.eop.common.component.lexicalknowledge.LexicalRule;
+import eu.excitementproject.eop.common.component.lexicalknowledge.RuleInfo;
+import eu.excitementproject.eop.common.configuration.CommonConfig;
+import eu.excitementproject.eop.common.configuration.NameValueTable;
+import eu.excitementproject.eop.common.exception.ConfigurationException;
+import eu.excitementproject.eop.common.utilities.configuration.ConfigurationFile;
+import eu.excitementproject.eop.common.utilities.configuration.ConfigurationParams;
+import eu.excitementproject.eop.core.component.lexicalknowledge.wordnet.WordnetLexicalResource;
+import eu.excitementproject.eop.distsim.resource.SimilarityStorageBasedLexicalResource;
+import eu.excitementproject.eop.lap.implbase.LAP_ImplBase;
+
+
+/**
+ * Produces alignment links between the text and the hypothesis,
+ * based on lexical rules: if T contains a 
+ * <P>
+ * Usage: First call {@link #init()} method, than start aligning
+ * any sentences pair.
+ * When the {@linkplain Aligner} object is no longer to be used, the
+ * {@link #cleanUp()} method should be called.
+ * <P>
+ * To align sentences: call {@link #align(String, String)} method.
+ * <P>
+ * 
+ * @author Vered Shwartz
+ * @since 26/05/2014
+ */
+public class LexicalAligner implements AlignmentComponent {
+
+	// Constants
+	private static final String LEXICAL_RESOURCES_CONF_SECTION = "LexicalResources";
+	private static final String WORDNET = "eu.excitementproject.eop.core.component.lexicalknowledge.wordnet";
+	private static final String REDIS_BAP = 
+			"eu.excitementproject.eop.core.component.lexicalknowledge.similarity.SimilarityStorageBasedLexicalResource";
+	
+	// Private Members
+	List<Token> textTokens;
+	List<Token> hypoTokens;
+	List<LexicalResource<? extends RuleInfo>> lexicalResources;
+	
+	// Public Methods
+	
+	/**
+	 * Align the text and the hypothesis.
+	 * <P>
+	 * This method receives a JCAS object containing two views:
+	 * Hypothesis and text views. The method assumes that the views
+	 * were already annotated with a tokenizer.
+	 * <P>
+	 * The lexical aligner looks at every phrase t in the text and every phrase
+	 * h in the hypothesis, and uses the lexical resources to find rules with
+	 * lhs = t and rhs = h.  
+	 * @param aJCas the JCAS object with the text and hypothesis view.
+	 * @throws AlignerRunException 
+	 */
+	@Override
+	public void annotate(JCas aJCas) {
+		
+		try {
+
+			// Get the tokens of the text and hypothesis
+			getTokensAnnotations(aJCas);
+			
+			// For every phrase t in T and phrase h in H, check the lexical
+			// resources if they contain a rule t->h
+			String textPhrase = "", hypoPhrase = "";
+			
+			for (int textStart = 0; textStart < textTokens.size(); ++textStart) {
+				for (int textEnd = textStart; textEnd < textTokens.size(); ++textEnd) {
+					textPhrase = getPhrase(textTokens, textStart, textEnd);
+					
+					for (int hypoStart = 0; hypoStart < hypoTokens.size(); ++hypoStart) {
+						for (int hypoEnd = hypoStart; hypoEnd < hypoTokens.size(); ++hypoEnd) {
+							hypoPhrase = getPhrase(hypoTokens, hypoStart, hypoEnd);
+							
+							// Get the rules textPhrase -> hypoPhrase
+							for (LexicalRule<? extends RuleInfo> rule : 
+									getRules(textPhrase, hypoPhrase)) {
+								
+								// Add alignment annotations
+								addAlignmentAnnotations(aJCas, textStart, textEnd, 
+										hypoStart, hypoEnd, rule);
+							}
+						}
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			
+			// TODO: Write to log / raise exception
+			System.out.println(this.getClass().getName() + 
+					"LexicalAligner failed aligning the sentence pair. " + 
+					e.getMessage());
+		} 		
+	}
+
+	@Override
+	public String getComponentName() {
+
+		// Name of this component that is used to identify the related configuration section
+		return this.getClass().getName(); 
+	}
+
+	@Override
+	public String getInstanceName() {
+		
+		// This component does not support instance configuration 
+		return null; 
+	} 
+
+	/**
+	 * Call this method once before starting to align sentence pairs.
+	 * @param config a CommonConfig instance. The aligner retrieves the lexical 
+	 * resources configuration values. 
+	 * @throws AlignerRunException if initialization failed
+	 * @throws ConfigurationException if the configuration is invalid
+	 */
+	public void init(CommonConfig config) throws AlignerRunException, ConfigurationException {
+		
+		// Get the relevant configuration section
+		NameValueTable lexicalResourcesSection = null;
+		try {
+			lexicalResourcesSection = config.getSection(LEXICAL_RESOURCES_CONF_SECTION);
+		} catch (ConfigurationException e) {
+			throw new ConfigurationException(e);
+		}
+		
+		lexicalResources = new ArrayList<LexicalResource<? extends RuleInfo>>();
+		
+		try {
+			
+			ConfigurationFile configFile = new ConfigurationFile(config);
+			
+			// Get each resource and create it using the configuration section related to it
+			for (String resourceClassName : lexicalResourcesSection.keySet()) {
+				lexicalResources.add(createLexicalResource(resourceClassName, 
+						configFile.getModuleConfiguration(resourceClassName)));
+			}
+	
+		} catch (LexicalResourceException e) {
+			throw new AlignerRunException("Could not load resources", e);
+		}
+	}
+	
+	/**
+	 * Cleans up any resources that were used by the aligner.
+	 * <P>
+	 * Call this method when the aligner is no longer to be used.
+	 */
+	public void cleanUp() {
+
+		// Close the lexical resources
+		for (LexicalResource<? extends RuleInfo> lexicalResource : lexicalResources) {
+			try {
+				lexicalResource.close();
+			} catch (LexicalResourceCloseException e) {
+				// TODO: Write to log
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// Private Methods
+	
+	/**
+	 * Uses the annotations in the CAS and extracts the tokens of the
+	 * text and hypothesis.
+	 * @param aJCas The JCas object of the text and hypothesis, after tokenization.
+	 * @throws CASException
+	 */
+	private void getTokensAnnotations(JCas aJCas) throws CASException {
+		
+		// Get the text and hypothesis views
+		JCas textView = aJCas.getView(LAP_ImplBase.TEXTVIEW);
+		JCas hypoView = aJCas.getView(LAP_ImplBase.HYPOTHESISVIEW);
+			
+		// Get the text and hypothesis tokens
+		textTokens = new ArrayList<Token>(JCasUtil.select(textView, Token.class));
+		hypoTokens = new ArrayList<Token>(JCasUtil.select(hypoView, Token.class));
+	}
+
+	/**
+	 * Get a phrase from a list of consecutive tokens
+	 * @param tokens The list of tokens
+	 * @param start The start token index
+	 * @param end The end token index
+	 * @return The phrase containing the tokens from start to end
+	 */
+	private String getPhrase(List<Token> tokens, int start, int end) {
+
+		StringBuilder phrase = new StringBuilder();
+		
+		for (Token token : tokens.subList(start, end + 1)) {
+			phrase.append(token.getCoveredText());
+		}
+		
+		return phrase.toString();
+	}
+
+	/**
+	 * Get rules of type textPhrase -> hypoPhrase, using all the lexical resources
+	 * in use by this aligner
+	 * @param textPhrase The text phrase, will be looked for as lhs of a rule
+	 * @param hypoPhrase The hypothesis phrase, will be looked for as rhs of a rule
+	 * @return The list of rules textPhrase -> hypoPhrase
+	 * @throws LexicalResourceException 
+	 */
+	private List<LexicalRule<? extends RuleInfo>> 
+						getRules(String textPhrase, String hypoPhrase) 
+								throws LexicalResourceException {
+
+		List<LexicalRule<? extends RuleInfo>> rules = 
+				new ArrayList<LexicalRule<? extends RuleInfo>>();
+		
+		// Check in all the resources for rules of type textPhrase -> hypoPhrase 
+		for (LexicalResource<? extends RuleInfo> resource : lexicalResources) {
+			
+			// TODO: What should I do with POS?
+			rules.addAll(resource.getRules(textPhrase, null, hypoPhrase, null));
+		}
+		
+		return rules;
+	}
+
+	/**
+	 * Add an alignment link from T to H, based on the rule t->h
+	 * in which t is a phrase in T from index textStart to textEnd of the tokens,
+	 * and h is a phrase in H from index hypoStart to hypoEnd of the tokens,
+	 * @param aJCas The JCas object
+	 * @param textStart The index of the first token in T in this alignment link 
+	 * @param textEnd The index of the last token in T in this alignment link 
+	 * @param hypoStart The index of the first token in H in this alignment link 
+	 * @param hypoEnd The index of the last token in H in this alignment link 
+	 * @param rule The rule t->h
+	 * @throws CASException 
+	 */
+	private void addAlignmentAnnotations(JCas aJCas, 	int textStart, int textEnd, 
+														int hypoStart, int hypoEnd, 
+														LexicalRule<? extends RuleInfo> rule) 
+																throws CASException {
+		// Get the text and hypothesis views
+		JCas textView = aJCas.getView(LAP_ImplBase.TEXTVIEW);
+		JCas hypoView = aJCas.getView(LAP_ImplBase.HYPOTHESISVIEW);
+		
+		// Prepare the Target instances
+		Target textTarget = new Target(textView);
+		Target hypoTarget = new Target(hypoView);
+		
+		// Prepare an FSArray instance and put the target annotations in it   
+		FSArray textAnnots = new FSArray(textView, textEnd - textStart + 1);
+		FSArray hypoAnnots = new FSArray(hypoView, hypoEnd - hypoStart + 1);
+		
+		int tokenIndex = 0;
+		for (Token token : textTokens.subList(textStart, textEnd)) {
+			textAnnots.set(tokenIndex++, token);
+		}
+		
+		tokenIndex = 0;
+		for (Token token : hypoTokens.subList(hypoStart, hypoEnd)) {
+			hypoAnnots.set(tokenIndex++, token);
+		}
+		
+		textTarget.setTargetAnnotations(textAnnots);
+		hypoTarget.setTargetAnnotations(hypoAnnots);
+		
+		// Set begin and end value of the Target annotations
+		textTarget.setBegin(textStart);
+		textTarget.setEnd(textEnd);
+		hypoTarget.setBegin(hypoStart);
+		hypoTarget.setEnd(hypoEnd);
+		
+		// Add the targets to the indices 
+		textTarget.addToIndexes(); 
+		hypoTarget.addToIndexes();
+		
+		// Mark an alignment.Link and add it to the hypothesis view
+		Link link = new Link(hypoView); 
+		link.setTSideTarget(textTarget); 
+		link.setHSideTarget(hypoTarget); 
+		
+		// TODO: Get the direction according to the resource
+		// and maybe set it as Bi-directional if h->t by the resource 
+		link.setDirection(Direction.TtoH); 
+		
+		// Set strength according to the rule data
+		link.setStrength(rule.getConfidence()); 
+		
+		// Add the link information
+		link.setAlignerID(rule.getResourceName());  
+		
+		// TODO: Set these fields differently?
+		link.setAlignerVersion("1.0"); 
+		link.setLinkInfo("local-entailment");
+		
+		// Mark begin and end according to the hypothesis target
+		link.setBegin(hypoTarget.getBegin()); 
+		link.setEnd(hypoTarget.getBegin());
+		
+		// Add to index 
+		link.addToIndexes(); 
+	}
+	
+	/**
+	 * Constructs a {@link LexicalResource} for the given class name
+	 * and a configuration subsection with parameters related to it. 
+	 * 
+	 * This function is allowed to return null.<BR>
+	 * The caller must check if the return value is null.
+	 * 
+	 * @param resourceClassName The class name of the lexical resource to load
+	 * @param configurationParams The {@link ConfigurationParams} object related
+	 * to the specific lexical resources.
+	 * @return
+	 * @throws ConfigurationException
+	 * @throws LexicalResourceException
+	 */
+	public LexicalResource<? extends RuleInfo> 
+		createLexicalResource(String resourceClassName, 
+				ConfigurationParams configurationParams) 
+				throws ConfigurationException, LexicalResourceException
+	{
+		LexicalResource<? extends RuleInfo> lexicalResource = null;
+		
+		// TODO: Implement all resources
+		switch (resourceClassName) {
+//			case BAP:
+//				ret = new Direct200LexicalResource(params);
+//				break;
+//			case CATVAR:
+//				ret = new CatvarLexicalResource(params);
+//				break;
+//			case VERB_OCEAN:
+//				ret = new VerbOceanLexicalResource(params);
+//				break;
+//			case WIKIPEDIA:
+//				ret = new WikiLexicalResource(params);
+//				break;
+//			case WIKIPEDIA_NEW:
+//				ret = new  eu.excitementproject.eop.lexicalminer.LexiclRulesRetrieval.WikipediaLexicalResource(params);
+//				break;
+				
+			// WordNet
+			case WORDNET: {
+				lexicalResource = new WordnetLexicalResource(configurationParams);
+				break;
+			}
+//			case LIN_DEPENDENCY_ORIGINAL:
+//				ret = new LinDependencyOriginalLexicalResource(params);
+//				break;
+//			case LIN_PROXIMITY_ORIGINAL:
+//				ret = new LinProximityOriginalLexicalResource(params);
+//				break;
+//			case LIN_DEPENDENCY_REUTERS:
+//				ret = new LinDistsimLexicalResource(params);
+//				break;
+//			case REDIS_LIN_PROXIMITY:
+//			case REDIS_LIN_DEPENDENCY:
+			
+			// Redis BAP
+			case REDIS_BAP: {
+				try {
+					lexicalResource = new 
+							SimilarityStorageBasedLexicalResource(configurationParams);
+				} catch (Exception e) {
+					throw new LexicalResourceException(e.toString());
+				}
+				break;
+			}
+			
+//			case REDIS_WIKI:
+//				try {
+//					ret = new RedisBasedWikipediaLexicalResource(params);
+//				} catch (Exception e) {
+//					throw new LexicalResourceException(e.toString());
+//				}
+//				break;
+//			case REDIS_GEO:
+//				try {
+//					ret = new RedisBasedGeoLexicalResource(params);
+//				} catch (Exception e) {
+//					throw new LexicalResourceException(e.toString());
+//				}
+//				break;								
+			default: {
+				lexicalResource = null;
+				break;
+			}
+		}
+
+		return lexicalResource;
+	}
+}
---
+++ core/src/test/java/eu/excitementproject/eop/core/alignment/LexicalAlignerTest.java
@@ -0,0 +1,73 @@
+package eu.excitementproject.eop.core.alignment;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.uimafit.util.JCasUtil;
+
+import eu.excitement.type.alignment.Link;
+import eu.excitementproject.eop.common.utilities.configuration.ImplCommonConfig;
+import eu.excitementproject.eop.lap.LAPAccess;
+import eu.excitementproject.eop.lap.biu.uima.BIUFullLAP;
+import eu.excitementproject.eop.lap.implbase.LAP_ImplBase;
+
+/**
+ * Test class to {@link eu.excitementproject.eop.core.alignment.LexicalAligner}.
+ * @author Vered Shwartz
+ *
+ */
+public class LexicalAlignerTest {
+
+	static Logger logger = Logger.getLogger(LexicalAligner.class.getName());
+	
+	@Test
+	public void test() {
+		
+		try {
+			
+			// Create a sentence pair example and annotate with tokens			
+			LAPAccess lap = new BIUFullLAP(
+					"src/test/resources/model/left3words-wsj-0-18.tagger",
+					"src/test/resources/model/ner-eng-ie.crf-3-all2008-distsim.ser.gz",
+					"localhost",
+					8080);
+			
+			JCas aJCas = lap.generateSingleTHPairCAS("death penalty", "capital punishment");
+//			JCas aJCas = lap.generateSingleTHPairCAS("I have two parrots.", "I own a bird.");
+//			JCas aJCas = lap.generateSingleTHPairCAS("I have a dog.", "I own a pet.");
+			
+			// Create and initialize the aligner
+			LexicalAligner aligner = new LexicalAligner();
+			File configFile = new File("src/test/resources/configuration-file/LexicalAligner_EN.xml");
+			ImplCommonConfig commonConfig = new ImplCommonConfig(configFile);
+			aligner.init(commonConfig);
+			
+			// Call the aligner to align T and H
+			aligner.annotate(aJCas);
+			
+			// Show the alignments of T and H
+			JCas hypoView = aJCas.getView(LAP_ImplBase.HYPOTHESISVIEW);
+			for (Link link : JCasUtil.select(hypoView, Link.class)) {
+				
+				System.out.println(String.format("Text start: %d, text end: %d, " +
+									"hypothesis start: %d, hypothesis end: %d, " + 
+									"resource: %s, confidence: %f", 
+									link.getTSideTarget().getBegin(), 
+									link.getTSideTarget().getEnd(), 
+									link.getHSideTarget().getBegin(), 
+									link.getHSideTarget().getEnd(), 
+									link.getAlignerID(),
+									link.getStrength()));
+			}
+			
+			// Dispose the aligner
+			aligner.cleanUp();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
