e5e792f605ef31fcac74815cbb9495894f1a7fd0
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-06-03 15:32:01 +0200

    some work on meteor phrase table

--- core/src/main/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseTable.java
+++ core/src/main/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseTable.java
@@ -1,32 +1,60 @@
 package eu.excitementproject.eop.core.component.alignment.phraselink;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map; 
+import java.util.HashMap; 
 
 import org.apache.log4j.Logger;
 
 /**
  * This class represents Meteor Phrase Table. 
  * 
+ * Memo: 
+ *    TODO consider (later) this aspect. -- force symmetric? for the moment, no. 
+ *   - one interesting aspect is that Meteor Phrase Table is *not* symmetric. 
+ *   - (e.g. "12 candidate countries -> 12 candidates does exist", but not the other way around, etc)
+ * 
  * @author Tae-Gil Noh
  *
  */
 public class MeteorPhraseTable {
-	
+
 	public MeteorPhraseTable(String resourcePath) throws IOException
 	{
+		// initialize private final variables 
 		logger = Logger.getLogger(this.getClass().toString()); 
+		entryPairsAsMap = new HashMap<String,Map<String,Float>>(); 
 
-		// TODO load up the resource. (in memory?) 
-		logger.debug("Loading Meteor Paraphrase table from resource path: " + resourcePath); 
+		// start loading the table text from resource path
+		logger.info("Loading Meteor Paraphrase table from resource path: " + resourcePath); 
 
-		
+		InputStream is = getClass().getResourceAsStream(resourcePath);
+		BufferedReader tableReader = new BufferedReader(new InputStreamReader(is)); 
+
+		int lc = 0; 
+		int ec = 0; 
+		String line = null; 
+		while((line = tableReader.readLine()) != null)
+		{
+			// TODO fill up the data structure
+			line.length(); 
+			lc++;
+		}
+		ec = lc / 3; 
+		logger.info("loading complelte, " + lc + " lines (" + ec + " entries).") ; 
+
 	}
 
 	/**
 	 * Query the table; return possible paraphrases for the given phrase with score.  
 	 * 
+	 * Give phrase is treated as "Left". (LHS ->) and the returning values are 
+	 * (RHS, probability)
 	 * 
 	 * @param phrase
 	 * @return
@@ -36,12 +64,12 @@ public class MeteorPhraseTable {
 		ArrayList<ScoredString> phrList = new ArrayList<ScoredString>(); 
 
 		// TODO Do matching and fill phrList 
-		
-		
-		
+
+
+
 		return phrList; 
 	}
-	
+
 	/**
 	 * A simple class that represents a tuple of String (that holds a phrase)
 	 * and its score. 
@@ -58,21 +86,25 @@ public class MeteorPhraseTable {
 			this.string = phrase; 
 			this.score = score; 
 		}
-		
+
 		public double getScore()
 		{
 			return score; 
 		}
-		
+
 		public String getString()
 		{
 			return string; 
 		}
-		
+
 		private final double  score; 
 		private final String string; 
 	}
-	
+
+	// internal data structures and variables 
+	private final Map<String,Map<String,Float>> entryPairsAsMap; 
 	private final Logger logger; 
+	
+	
 
 }
--- core/src/test/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseTableTest.java
+++ core/src/test/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseTableTest.java
@@ -2,16 +2,35 @@ package eu.excitementproject.eop.core.component.alignment.phraselink;
 
 import static org.junit.Assert.*;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class MeteorPhraseTableTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		// Set Log4J for the test 
+		BasicConfigurator.resetConfiguration(); 
+		BasicConfigurator.configure(); 
+		Logger.getRootLogger().setLevel(Level.DEBUG);   
+		@SuppressWarnings("unused")
+		Logger testlogger = Logger.getLogger(this.getClass().toString()); 
 		
 		// TODO 
 		// load test. 
+		MeteorPhraseTable englishTable = null; 
+		try 
+		{
+			englishTable = new MeteorPhraseTable("/meteor-1.5/data/paraphrase-en"); 
+			
+		}
+		catch (Exception e)
+		{
+			fail(e.getMessage()); 
+		}
 		
 		// read test for known value.  
 		
