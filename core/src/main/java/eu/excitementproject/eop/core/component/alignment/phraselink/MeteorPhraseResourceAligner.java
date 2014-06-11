e414d942e8f44d818cb5f019ea34a99ecd259da4
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-06-11 15:11:38 +0200

    phrase candidate extraction utility method, MeteorPhraseResourcealigner

--- core/src/main/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseResourceAligner.java
+++ core/src/main/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseResourceAligner.java
@@ -1,8 +1,13 @@
 package eu.excitementproject.eop.core.component.alignment.phraselink;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.apache.uima.jcas.JCas;
+import org.uimafit.util.JCasUtil; 
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 import eu.excitementproject.eop.common.component.alignment.AlignmentComponent;
 import eu.excitementproject.eop.common.component.alignment.AlignmentComponentException;
@@ -37,7 +42,52 @@ public class MeteorPhraseResourceAligner implements AlignmentComponent {
 	{
 		
 	}
-	
+
+	/**
+	 * This method is a helper utility that is required to look up Meteor Phrase tables. 
+	 * 
+	 * Basically, returns all possible phrase candidates up to N words in a List<String> 
+	 * 
+	 * The method uses Token annotation in JCas to generate possible candidates. Thus, 
+	 * a tokenization annotator should have annotated this JCas before.  
+	 * 
+	 * @param JCas aJCas The view, that holds the sentence(s) to be analyzed. 
+	 * @param int uptoN The maximum number of 
+	 * @return 
+	 */
+	public static List<String> getPhraseCandidatesFromSOFA(JCas aJCas, int uptoN)
+	{
+		// sanity check 
+		assert(aJCas !=null); 
+		assert(uptoN > 0); 
+		
+		// list for result, 
+		List<String> result = new ArrayList<String>(); 
+		
+		// ok. work. first; 
+		// get all Tokens (by appearing orders...) 
+		Collection<Token> t = JCasUtil.select(aJCas, Token.class); 
+		Token[] tokens = t.toArray(new Token[t.size()]); 
+		
+		// then; 
+		// for each Token, start uptoN process. 
+		for(int i=0; i < tokens.length; i++)
+		{
+			for(int j=0; (j < uptoN) && (i+j < tokens.length); j++ )
+			{
+				Token leftEnd = tokens[i]; 
+				Token rightEnd = tokens[i+j]; 
+				String text = aJCas.getDocumentText().substring(leftEnd.getBegin(), rightEnd.getEnd()); 
+				// and store in lower case. 
+				result.add(text.toLowerCase()); 
+			}			
+		}
+		
+		// done 
+		// all candidates are store here. 
+		return result; 
+	}
+
 	public String getComponentName()
 	{
 		return this.getClass().getName(); 
@@ -47,6 +97,7 @@ public class MeteorPhraseResourceAligner implements AlignmentComponent {
 	{
 		return resourcePath; 
 	}
+	
 	
 	private final String resourcePath; 
 	@SuppressWarnings("unused")
--- core/src/test/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseResourceAlignerTest.java
+++ core/src/test/java/eu/excitementproject/eop/core/component/alignment/phraselink/MeteorPhraseResourceAlignerTest.java
@@ -2,22 +2,86 @@ package eu.excitementproject.eop.core.component.alignment.phraselink;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.uima.jcas.JCas;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.excitementproject.eop.common.component.alignment.AlignmentComponent;
+import eu.excitementproject.eop.lap.LAPAccess;
+import eu.excitementproject.eop.lap.dkpro.OpenNLPTaggerEN;
 
 @SuppressWarnings("unused")
 public class MeteorPhraseResourceAlignerTest {
 
-	@Ignore
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		BasicConfigurator.resetConfiguration(); 
+		BasicConfigurator.configure(); 
+		Logger.getRootLogger().setLevel(Level.DEBUG);   
+		Logger testlogger = Logger.getLogger(this.getClass().toString()); 
 		
-		AlignmentComponent a = null; 
-		// TODO 
+		// prepare a JCas 
+		JCas aJCas = null; 
+		OpenNLPTaggerEN tokenizer = null; 
+		try 
+		{
+			tokenizer = new OpenNLPTaggerEN(); 
+			aJCas = tokenizer.generateSingleTHPairCAS("This is a cat.", "This is China's new cat."); 
+		}
+		catch (Exception e)
+		{
+			fail(e.getMessage()); 
+		}
+
+		// phrase candidate extract test 		
+		try 
+		{   List<String> candidates = null; 
+			// from TEXTVIEW
+			candidates = MeteorPhraseResourceAligner.getPhraseCandidatesFromSOFA(aJCas.getView(OpenNLPTaggerEN.TEXTVIEW), 6); 
+			testlogger.debug(candidates.size() + " candidates found. They are; "); 
+			// should be 15 candidates 
+			assertEquals(candidates.size(), 15); 
+			for(String s : candidates)
+			{
+				testlogger.debug(s); 
+			}
+			// from HYPOTHESISVIEw 
+			candidates = MeteorPhraseResourceAligner.getPhraseCandidatesFromSOFA(aJCas.getView(OpenNLPTaggerEN.HYPOTHESISVIEW), 6); 
+			testlogger.debug(candidates.size() + " candidates found. They are; "); 
+			// should be 27 candidates 
+			assertEquals(candidates.size(), 27); 
+			for(String s : candidates)
+			{
+				testlogger.debug(s); 
+			}
+
+			// once more on HYPOTHESISVIEW, but with less uptoN. 
+			candidates = MeteorPhraseResourceAligner.getPhraseCandidatesFromSOFA(aJCas.getView(OpenNLPTaggerEN.HYPOTHESISVIEW), 4); 
+			testlogger.debug(candidates.size() + " candidates found. They are; "); 
+			// should be 22 candidates 
+			assertEquals(candidates.size(), 22); 
+			for(String s : candidates)
+			{
+				testlogger.debug(s); 
+			}
+			
+			// empty case. 
+			aJCas = tokenizer.generateSingleTHPairCAS("", "This is China's new cat."); 			
+			candidates = MeteorPhraseResourceAligner.getPhraseCandidatesFromSOFA(aJCas.getView(OpenNLPTaggerEN.TEXTVIEW), 4); 
+			// no candidates, but shouldn't make any exception. 
+			assertEquals(candidates.size(), 0); 
+		}
+		catch (Exception e)
+		{
+			fail(e.getMessage()); 
+		}
 		
+		// main class test. 
 		// load test 
 		
 		// alignment test with one CAS 
