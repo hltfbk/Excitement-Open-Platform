2c75b72cfcaa725166aae5730fc72d5951ed2d17
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-07-30 14:53:39 +0200

    started alignment EDAs as a separate sub module

---
+++ alignmentedas/src/main/java/eu/excitement/eop/alignmentedas/App.java
@@ -0,0 +1,13 @@
+package eu.excitement.eop.alignmentedas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
