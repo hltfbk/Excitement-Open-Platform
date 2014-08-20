a20e35c04fd204c063771b521da0a101b7b4944d
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2014-08-20 20:04:12 +0200

    outline of a simplest alignment evaluator...

---
+++ alignmentedas/src/main/java/eu/excitementproject/eop/alignmentedas/p1eda/scorers/SimpleWordCoverageCounter.java
@@ -0,0 +1,48 @@
+package eu.excitementproject.eop.alignmentedas.p1eda.scorers;
+
+import java.util.Vector;
+
+import org.apache.uima.jcas.JCas;
+
+import eu.excitementproject.eop.common.component.scoring.ScoringComponent;
+import eu.excitementproject.eop.common.component.scoring.ScoringComponentException;
+
+/**
+ * This is a very simple "alignment evaluator" which reports two numbers; 
+ * "number of covered content word in H", "number of content words in H".
+ * 
+ * @author Tae-Gil Noh
+ *
+ */
+public class SimpleWordCoverageCounter implements ScoringComponent {
+
+	/**
+	 *  Argument version: gets one linkID of alignment.Link. Then the module uses alignment.Link instances 
+	 *  with that ID, to calculate "coverage". 
+	 *  If null given, the module uses, *all* link instances to calculate coverage. 
+	 *  
+	 */
+	public SimpleWordCoverageCounter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getInstanceName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<Double> calculateScores(JCas cas)
+			throws ScoringComponentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ alignmentedas/src/main/java/eu/excitementproject/eop/alignmentedas/p1eda/scorers/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package holds some example Scorer ("Feature extractors", or "alignment evaluators", or JCas with alignment
+ * in and some features out) modules for P1EDA 
+ * 
+ * @author Tae-Gil Noh (day-dreaming about hot summer, Heidelberg, 2014. --- well, this 2014 summer feels rather like all October-s!)
+ */
+package eu.excitementproject.eop.alignmentedas.p1eda.scorers;
