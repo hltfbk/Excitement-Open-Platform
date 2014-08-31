bdbf85bb950c498355ab4db6e982715e7734e8d7
Madhumita <Madhumita@192.168.0.12>
2014-08-31 11:39:03 +0200

    added comments

--- core/src/main/java/eu/excitementproject/eop/core/component/alignment/nemex/NemexAligner.java
+++ core/src/main/java/eu/excitementproject/eop/core/component/alignment/nemex/NemexAligner.java
@@ -32,9 +32,28 @@ import de.dfki.lt.nemex.a.*;
 import de.dfki.lt.nemex.a.data.GazetteerNotLoadedException;
 
 /**
+ * 
+ * This class provides nemex.NemexType annotations for a given JCas pair and
+ * adds alignment.Link on nemex.NemexType.
+ * 
+ * Adds alignment from hypothesis to text annotations. An online Gazetteer is
+ * created based on queries from the hypothesis. The queries in text are
+ * annotated based on the online Gazetteer.
+ * 
+ * (This is the first version of the aligner and does not do efficient query
+ * generation and disambiguation. It simply creates all possible substrings as
+ * queries from hypothesis and text and adds all possible approximate
+ * alignments.)
+ * 
+ * Resource it is based on: Nemex-A tool developed at DFKI.
+ * 
+ * 
+ * 
  * @author Madhumita
+ * @since June 2014
  * 
  */
+
 public class NemexAligner implements AlignmentComponent {
 
 	public NemexAligner(String gazetteerFilePath, String delimiter,
@@ -55,7 +74,16 @@ public class NemexAligner implements AlignmentComponent {
 		// this.ignoreDuplicateNgrams);
 	}
 
-	@Override
+	/**
+	 * This method adds nemex.NemexType annotations between text and hypothesis
+	 * for a given JCas pair and then adds alignment.Link between two
+	 * nemex.NemexType targets.
+	 * 
+	 * @param JCas
+	 *            aJCas The view, that holds the sentence(s) to be analyzed.
+	 * @return
+	 */
+
 	public void annotate(JCas aJCas) throws PairAnnotatorComponentException {
 		// intro log
 		logger.info("annotate() called with a JCas with the following T and H;  ");
@@ -66,10 +94,8 @@ public class NemexAligner implements AlignmentComponent {
 					"annotate() got a null JCas object.");
 		}
 
-		// unique query id vs query string
 		HashMap<Integer, String> queryMap = new HashMap<Integer, String>();
 
-		// query string vs hypothesis views and offsets
 		HashMap<String, ArrayList<QueryOffset>> queryIndex = new HashMap<String, ArrayList<QueryOffset>>();
 
 		try {
@@ -93,6 +119,26 @@ public class NemexAligner implements AlignmentComponent {
 
 	}
 
+	/**
+	 * This method creates an online Gazetteer from queries in H, which is used
+	 * to lookup aligned T queries.
+	 * 
+	 * After creating the Gazetteer, it loads it to the NemexA system.
+	 * 
+	 * It also annotates the H queries with nemex.NemexType.
+	 * 
+	 * @param JCas
+	 *            hypoView The hypothesis view, that holds the hypothesis to be
+	 *            analyzed.
+	 * @param HashMap
+	 *            <Integer, String> queryMap The map of unique query Id and
+	 *            query (dictionary entry) String
+	 * @param HashMap
+	 *            <String, ArrayList<QueryOffset>> queryIndex Inverted index
+	 *            from queries to the hypotheses and offsets which generate the
+	 *            query.
+	 * @return
+	 */
 	public void createDictionary(JCas hypoView,
 			HashMap<Integer, String> queryMap,
 			HashMap<String, ArrayList<QueryOffset>> queryIndex)
@@ -138,9 +184,10 @@ public class NemexAligner implements AlignmentComponent {
 		PrintWriter fw;
 		try {
 			fw = new PrintWriter(new FileWriter(this.gazetteerFilePath));
-			fw.println("0 utf-8 EN " + (int)totalNoOfQueries + " " + queryMap.size());
+			fw.println("0 utf-8 EN " + (int) totalNoOfQueries + " "
+					+ queryMap.size());
 			fw.close();
-			
+
 			fw = new PrintWriter(new FileWriter(this.gazetteerFilePath, true));
 			while (iter.hasNext()) {
 
@@ -156,10 +203,12 @@ public class NemexAligner implements AlignmentComponent {
 
 				List<String> values = new ArrayList<String>();
 				values.add(queryText);
-				
+
 				String entry = new String();
-				entry = new String(idx + " " + Math.log(value.size()/totalNoOfQueries) + " " + queryText
-						+ " " + "NG:" + "1:" +Math.log(value.size()/totalNoOfQueries));
+				entry = new String(idx + " "
+						+ Math.log(value.size() / totalNoOfQueries) + " "
+						+ queryText + " " + "NG:" + "1:"
+						+ Math.log(value.size() / totalNoOfQueries));
 
 				logger.info("Adding entry to dictionary," + entry);
 
@@ -184,12 +233,31 @@ public class NemexAligner implements AlignmentComponent {
 					this.delimiterSwitchOff, this.nGramSize,
 					this.ignoreDuplicateNgrams);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			logger.info("Error updating the Gazetteer file");
 			e.printStackTrace();
 		}
 
 	}
 
+	/**
+	 * This method adds nemex.NemexType annotation on text queries.
+	 * 
+	 * It generates queries from text string. Approximate similar matches of
+	 * queries are looked up using the Gazetteer created by the hypotheses.
+	 * 
+	 * The similar matches are added as values to nemex.NemexType annotation.
+	 * 
+	 * @param JCas
+	 *            textView The text view, which holds the text to be analyzed.
+	 * @param HashMap
+	 *            <Integer, String> queryMap The map of unique query Id and
+	 *            query (dictionary entry) String
+	 * @param HashMap
+	 *            <String, ArrayList<QueryOffset>> queryIndex Inverted index
+	 *            from queries to the hypotheses and offsets which generate the
+	 *            query.
+	 * @return
+	 */
 	private void annotateSubstring(JCas textView,
 			HashMap<Integer, String> queryMap,
 			HashMap<String, ArrayList<QueryOffset>> queryIndex) {
@@ -212,12 +280,27 @@ public class NemexAligner implements AlignmentComponent {
 								queryIndex);
 					}
 				} catch (GazetteerNotLoadedException e) {
+					logger.info("Could not load the gazetteer");
 					e.printStackTrace();
 				}
 			}
 
 	}
 
+	/**
+	 * This method adds nemex.NemexType annotation on queries.
+	 * 
+	 * Annotations with on a given view, startOffset, endOffset and value are
+	 * added to the index.
+	 * 
+	 * @param JCas
+	 *            view The view which contains the text to be annotated
+	 * @param List
+	 *            <String> entry The values of the nemex.NemexType annotation
+	 * @param int startOffset The startOffset of the nemex.NemexType annotation
+	 * @param int endOffset The endOffset of the nemex.NemexType annotation
+	 * @return NemexType The added nemex.NemexType annotation
+	 */
 	private NemexType addNemexAnnotation(JCas view, List<String> entry,
 			int startOffset, int endOffset) {
 
@@ -237,6 +320,8 @@ public class NemexAligner implements AlignmentComponent {
 
 			logger.info("Setting values of annotation");
 			annot.setValues(valuesArray);
+
+			// add annotation to index of annotations
 			annot.addToIndexes();
 			return annot;
 		} catch (Exception e) {
@@ -248,6 +333,27 @@ public class NemexAligner implements AlignmentComponent {
 
 	}
 
+	/**
+	 * This method adds alignment.Link annotation on nemex.NemexType.
+	 * 
+	 * Two targets are generated - one on text and hypothesis view each. The two
+	 * targets are linked by alignment.Link from H to T.
+	 * 
+	 * @param NemexType
+	 *            textAnnot The text annotation for text Target
+	 * @param JCas
+	 *            textView The JCas view which contains the text of a given pair
+	 * @param int tStart The start offset of text annotation textAnnot
+	 * @param int tEnd The end offset of text annotation textAnnot
+	 * @param HashMap
+	 *            <Integer, String> queryMap The map of unique query Id and
+	 *            query (dictionary entry) String
+	 * @param HashMap
+	 *            <String, ArrayList<QueryOffset>> queryIndex Inverted index
+	 *            from queries to the hypotheses and offsets which generate the
+	 *            query.
+	 * @return
+	 */
 	private void addAlignmentLink(NemexType textAnnot, JCas textView,
 			int textStart, int textEnd, HashMap<Integer, String> queryMap,
 			HashMap<String, ArrayList<QueryOffset>> queryIndex) {
@@ -273,39 +379,45 @@ public class NemexAligner implements AlignmentComponent {
 
 	}
 
+	/**
+	 * This method adds alignment.Link annotation between text and hypothesis
+	 * target
+	 * 
+	 * @param JCas
+	 *            tView The JCas view which contains the text of a given pair
+	 * @param int tStart The start offset of the text annotation to be used as
+	 *        Target
+	 * @param int tEnd The end offset of text annotation to be used as Target
+	 * @param JCas
+	 *            hView The JCas view which contains the hypothesis of a given
+	 *            pair
+	 * @param int hStart The start offset of hypothesis annotation to be used as
+	 *        Target
+	 * @param int hEnd The end offset of the hypothesis annotation to be used as
+	 *        Target
+	 * @return
+	 */
+
 	private void addLink(JCas tView, int tStart, int tEnd, JCas hView,
 			int hStart, int hEnd) {
+
 		// Prepare the Target instances
 		Target textTarget = new Target(tView);
 		Target hypoTarget = new Target(hView);
 
 		for (NemexType ntype : JCasUtil.select(tView, NemexType.class)) {
-			// Actual work code to put Annotations
-			// in a Target instance.
 
-			// 1) prepare a Target instance.
 			if ((ntype.getBegin() == tStart) && (ntype.getEnd() == tEnd)) {
 				Target tg = new Target(tView);
-				// 2) prepare a FSArray instance, put the target annotations in
-				// it.
-				FSArray tAnnots = new FSArray(tView, 1); // this is a size 1
-															// FSarray;
+
+				FSArray tAnnots = new FSArray(tView, 1);
 				tAnnots.set(0, ntype);
-				// 3) Okay, now the FSArray is prepared. Put it on field
-				// "targetAnnotations"
+
 				tg.setTargetAnnotations(tAnnots);
-				// 4) Set begin - end value of the Target annotation (just like
-				// any annotation)
-				// note that, setting of begin and end of Target is a
-				// convention.
-				// - begin as the earliest "begin" (among Target-ed annotations)
-				// - end as the latest "end" (among Target-ed annotations)
 				tg.setBegin(ntype.getBegin());
 				tg.setEnd(ntype.getEnd());
-				// 5) add it to the index (just like any annotation)
 				tg.addToIndexes();
 
-				// The target instance is now ready.
 				textTarget = tg;
 			}
 		}
--- core/src/main/java/eu/excitementproject/eop/core/component/alignment/nemex/QueryOffset.java
+++ core/src/main/java/eu/excitementproject/eop/core/component/alignment/nemex/QueryOffset.java
@@ -2,29 +2,39 @@ package eu.excitementproject.eop.core.component.alignment.nemex;
 
 import org.apache.uima.jcas.JCas;
 
+/**
+ * 
+ * This class is used to store the information for any query. It stores the view
+ * of the hypothesis which generates a query, along with start and end offsets
+ * of the query.
+ * 
+ * @author Madhumita
+ * @since June 2014
+ * 
+ */
+
 public class QueryOffset {
-	
+
 	JCas hypoView;
-	int startOffset; //inclusive
-	int endOffset; //exclusive
-	
+	int startOffset; // inclusive
+	int endOffset; // exclusive
+
 	public QueryOffset(JCas hView, int start, int end) {
-		hypoView = hView; 
+		hypoView = hView;
 		startOffset = start;
 		endOffset = end;
 	}
-	
+
 	public JCas getHypothesisView() {
 		return hypoView;
 	}
-	
+
 	public int getStartOffset() {
 		return startOffset;
 	}
-	
+
 	public int getEndOffset() {
 		return endOffset;
 	}
-	
 
 }
